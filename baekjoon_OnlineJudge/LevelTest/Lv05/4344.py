29e0dd45588058844a1443f8f1617ba70232b3a7
swj1265 <swj1265@naver.com>
2021-05-20 13:41:44 +0900

    20210520

--- baekjoon_OnlineJudge/LevelTest/Lv05/4344.py
+++ baekjoon_OnlineJudge/LevelTest/Lv05/4344.py
@@ -2,3 +2,17 @@ import sys
 
 C = int(sys.stdin.readline().rstrip())
 
+for _ in range(C):
+    case = list(map(int, sys.stdin.readline().rstrip().split()))
+    total = sum(case[1:])
+    N = case[0]
+    Avg = total / N
+    count = 0
+
+    for i in range(1, N + 1):
+        if case[i] > Avg:
+            count += 1
+    rate = (count/N)*100
+    print(f'{rate:.3f}%')
+
+

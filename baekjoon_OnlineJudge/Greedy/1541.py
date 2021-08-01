482cc7b571d839eddfd874c0ac576f7bd02ee4a0
swj1265 <swj1265@naver.com>
2021-08-01 15:27:44 +0900

    20210801

---
+++ baekjoon_OnlineJudge/Greedy/1541.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda : sys.stdin.readline().rstrip()
+
+a = input().split('-')
+
+data = []
+
+for i in a:
+    count = 0
+    b = i.split('+')
+    for c in b:
+        count += int(c)
+    data.append(count)
+
+result = data[0]
+
+for i in range(1, len(data)):
+    result -= data[i]
+print(result)

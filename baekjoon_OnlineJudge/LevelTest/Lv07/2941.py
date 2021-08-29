b4946ae706cab1cd0c5a3539214d2d02170b29c3
swj1265 <swj1265@naver.com>
2021-08-29 23:33:21 +0900

    20210829

--- baekjoon_OnlineJudge/LevelTest/Lv07/2941.py
+++ baekjoon_OnlineJudge/LevelTest/Lv07/2941.py
@@ -2,3 +2,11 @@ import sys
 
 input = lambda : sys.stdin.readline().rstrip()
 
+alphabet = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+
+X = input()
+
+for i in alphabet:
+    X = X.replace(i, '*')
+
+print(len(X))

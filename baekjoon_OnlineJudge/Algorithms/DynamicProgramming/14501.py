70211670e82b315b472cb8f81ef6d791b5195945
swj1265 <swj1265@naver.com>
2021-09-02 23:22:57 +0900

    20210902

---
+++ baekjoon_OnlineJudge/Algorithms/DynamicProgramming/14501.py
@@ -0,0 +1,20 @@
+import sys ; input = lambda : sys.stdin.readline().rstrip()
+
+N = int(input())
+T = []
+P = []
+dp = []
+for _ in range(N):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+    dp.apend(p)
+dp.append(0)
+
+for i in range(N-1, -1, -1):
+    if T[i] + i > N:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], p[i] + dp[i + T[i]])
+print(dp[0])
+    

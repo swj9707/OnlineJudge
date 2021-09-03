7118a6d643cf13736daf11fda5ea0889b6e4ee18
swj1265 <swj1265@naver.com>
2021-09-03 18:40:41 +0900

    20210903

--- baekjoon_OnlineJudge/Algorithms/Brute Force/10972.py
+++ baekjoon_OnlineJudge/Algorithms/Brute Force/10972.py
@@ -2,6 +2,13 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 import itertools
 
 N = int(input())
-inputData = list(map(int, input().split()))
-data = [i for i in range(1,N+1)]
-
+inputData = tuple(map(int, input().split()))
+data = [i for i in range(1, N+1)]
+combi = itertools.permutations(data, N)
+flag = 0
+for i in combi:
+    if flag == 1:
+        print(i)
+        break
+    if inputData == i:
+        flag = 1

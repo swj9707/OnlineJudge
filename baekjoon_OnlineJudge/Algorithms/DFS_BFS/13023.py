6a52c1a117b95d169ffe2980ff236b1093a012d1
swj1265 <swj1265@naver.com>
2021-09-05 03:57:14 +0900

    20210905

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/13023.py
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/13023.py
@@ -6,21 +6,19 @@ N, M = map(int, input().split())
 matrix = [[0] * (N+1) for i in range(N+1)]
 visit_list = [0]*(N+1)
 
+
 for _ in range(M):
     a, b = map(int, input().split())
     matrix[a][b] = matrix[b][a] = 1
 
-depth = 0
-def dfs(V):
-    global depth
-    if depth == 5:
+def dfs(V, number):
+    if number == 4:
         print(1)
         exit()
     visit_list[V] = 1
-    depth += 1
     for i in range(1, N+1):
         if(visit_list[i] == 0 and matrix[V][i]==1):
-            dfs(i)
+            dfs(i, number + 1)
           
-dfs(0)
+dfs(0, 0)
 print(0)

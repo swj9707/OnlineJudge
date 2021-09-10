04c5b410f3056c2d58a1d392e102beb949a7155c
swj1265 <swj1265@naver.com>
2021-09-10 17:31:25 +0900

    20210910

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/2178.py
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/2178.py
@@ -2,6 +2,24 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 
 N, M = map(int, input().split())
 
-Data = [list(map(int, input())) for _ in range(N)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+board = [list(input()) for _ in range(N)]
+board[0][0] = 1
+queue = [[0,0]]
+def bfs(queue, board):
+    while queue:
+        x, y = queue[0][0], queue[0][1]
+        del queue[0]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+            if nx >= 0 and ny >= 0 and nx < N and ny < M:
+                if board[nx][ny] == '1':
+                    queue.append([nx,ny])
+                    board[nx][ny] = board[x][y] + 1
 
+bfs(queue, board)
+
+print(board[N-1][M-1])

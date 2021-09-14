8a18cc5af18d27ded88600764d6b6ebc71665ea9
swj1265 <swj1265@naver.com>
2021-09-14 13:32:21 +0900

    20210913

--- baekjoon_OnlineJudge/Algorithms/Greedy/1931.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/1931.py
@@ -2,5 +2,10 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 
+data = []
 for _ in range(N):
+    data.append(list(map(int, input().split())))
+data = sorted(data, key = lambda x : x[0])
+data = sorted(data, key = lambda x : x[1])
 
+print(data)

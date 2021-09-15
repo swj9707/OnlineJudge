b742cbaffc9e4308d9973003665f180c805ba15e
swj1265 <swj1265@naver.com>
2021-09-15 16:54:16 +0900

    20210915

--- baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
@@ -2,7 +2,13 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 
-def chgLight(lights, N):
+def chgLights(A):
+    if A == '0':
+        A = '1'
+    else:
+        A = '0'
+
+def chgSwitch(lights, N):
     if N == 0:
         lights[N] = 1 - lights[N]
         lights[N + 1] = 1 - lights[N+1]

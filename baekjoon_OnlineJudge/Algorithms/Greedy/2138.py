eaecdaedb8d726b80493f90e1f13196ff46c0166
swj1265 <swj1265@naver.com>
2021-09-15 17:33:20 +0900

    20210915

--- baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
@@ -7,18 +7,19 @@ def chgLights(A):
         A = '1'
     else:
         A = '0'
+    return A
 
 def chgSwitch(lights, N):
     if N == 0:
-        chgLights(lightss[N])
-        chgLights(lights[N + 1])
+        Lights[N] = chgLights(lights[N])
+        Lights[N+1] = chgLights(lights[N + 1])
     elif N == len(lights):
-        chgLights(lights[len(lights)])
-        chgLights(lights[len(lights) - 1])
+        Lights[len(lights)] = chgLights(lights[len(lights)])
+        Lights[len(lights)-1] = chgLights(lights[len(lights) - 1])
     else:
-        chgLights(lights[N-1])
-        chgLights(lightss[N])
-        chgLights(lights[N + 1])
+        Lights[N-1] = chgLights(lights[N-1])
+        Lights[N] = chgLights(lights[N])
+        Lights[N+1] = chgLights(lights[N + 1])
 
 now = input()
 answer = input()

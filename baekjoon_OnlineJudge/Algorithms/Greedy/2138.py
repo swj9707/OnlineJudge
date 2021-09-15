7d00653bdb41e55e46f191216b070c611ccd2564
swj1265 <swj1265@naver.com>
2021-09-15 09:18:15 +0900

    20210915

--- baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
@@ -2,6 +2,18 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 
+def chgLight(lights, N):
+    if N == 0:
+        lights[N] = 1 - lights[N]
+        lights[N + 1] = 1 - lights[N+1]
+    elif N == len(lights):
+        lights[len(lights)] = 1 - lights[len(lights)]
+        lights[len(lights) - 1] = 1 - lights[len(lights) - 1]
+    else:
+        lights[N-1] = 1 - lights[N-1]
+        lights[N] = 1 - lights[N]
+        lights[N + 1] = 1 - lights[N+1]
+
 now = input()
 answer = input()
 

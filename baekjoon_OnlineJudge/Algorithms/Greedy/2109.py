0c0ff487c1bae42b6e5309703793ccda8f8dd147
swj1265 <swj1265@naver.com>
2021-09-17 02:23:24 +0900

    20210917

--- baekjoon_OnlineJudge/Algorithms/Greedy/2109.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/2109.py
@@ -1,6 +1,15 @@
 import sys ; input = lambda : sys.stdin.readline().rstrip()
+import heapq
 
 N = int(input())
 
 data = [list(map(int, input().split())) for _ in range(N)]
+data = sorted(data, key = lambda x : x[1])
+
+pList = []
+for i in data:
+    heapq.heappush(pList, i[0])
+    if len(pList) > i[1]:
+        heapq.heappop(pList)
+print(sum(pList))
 

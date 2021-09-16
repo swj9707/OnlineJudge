c8636061ecbd0fcf617c41e165db9ef9d33e78f0
swj1265 <swj1265@naver.com>
2021-09-16 16:02:48 +0900

    20210916

--- baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
+++ baekjoon_OnlineJudge/Algorithms/Greedy/2138.py
@@ -2,26 +2,48 @@ import sys ; input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 
-def chgLights(A):
-    if A == '0':
-        A = '1'
-    else:
-        A = '0'
-    return A
-
-def chgSwitch(lights, N):
+def chgSwitch(Lights, N):
     if N == 0:
-        Lights[N] = chgLights(lights[N])
-        Lights[N+1] = chgLights(lights[N + 1])
+        Lights[N] = str(1 - int(Lights[N]))
+        Lights[N+1] = str(1 - int(Lights[N + 1]))
     elif N == len(lights):
-        Lights[len(lights)] = chgLights(lights[len(lights)])
-        Lights[len(lights)-1] = chgLights(lights[len(lights) - 1])
+        Lights[len(lights)] = str(1 - int(Lights[len(Lights)]))
+        Lights[len(lights)-1] = str(1 - int(Lights[len(Lights) - 1]))
     else:
-        Lights[N-1] = chgLights(lights[N-1])
-        Lights[N] = chgLights(lights[N])
-        Lights[N+1] = chgLights(lights[N + 1])
+        Lights[N-1] = str(1 - int(Lights[N-1]))
+        Lights[N] = str(1 - int(Lights[N]))
+        Lights[N+1] = str(1 - int(Lights[N + 1]))
 
 now = input()
 answer = input()
 
+def first(Lights):
+    count = 0
+    chgSwitch(Lights, 0)
+    for i in range(1, len(Lights)):
+        if Lights[i- 1] == answer[i-1]:
+            continue
+        else:
+            count += 1
+            chgSwitch(Lights, i)
+    if Lights == answer:
+        return count
+    return -1
 
+def second(Lights):
+    count = 0
+    for i in range(1, len(Lights)):
+        if Lights[i- 1] == answer[i-1]:
+            continue
+        else:
+            count += 1
+            chgSwitch(Lights, i)
+    if Lights == answer:
+        return count
+    return -1
+
+answer = min(first(now), second(now))
+if answer == -1:
+    print(-1)
+else:
+    print(answer)

f90f2b262241c222a749cafe253bc3f837e760c5
swj1265 <swj1265@naver.com>
2021-09-24 01:28:41 +0900

    20210924

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/1012.cpp
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/1012.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#define MAX 50;
+#include <queue>
+#include <cstring>
+#define MAX 50
 
 using namespace std;
 
@@ -9,31 +11,49 @@ int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
 int M, N, K;
 
-void init() {
+void initSet() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 }
 
-void bfs(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
-		if (matrix[nx][ny] && !check[nx][ny]) {
-			check[nx][ny] = 1;
+void initData() {
+	for (int i = 0; i < MAX; i++) memset(matrix[i], 0, sizeof(int) * MAX);
+}
 
+bool DFS(int X, int Y) {
+	if (X < 0 || X >= M || Y < 0 || Y >= N) return false;
+	if (matrix[X][Y] == 1) {
+		matrix[X][Y] = 0;
+		for (int i = 0; i < 4; i++) {
+			int x = X + dx[i];
+			int y = Y + dy[i];
+			DFS(x, y);
 		}
+		return true;
 	}
+	return false;
 }
 
 int main() {
-	init();
+	initSet();
 	int T;
 	cin >> T;
-	cin >> M >> N >> K;
 	for (int i = 0; i < T; i++) {
-		int X, Y;
-		cin >> X >> Y;
-		matrix[X][Y] = 1;
+		cin >> M >> N >> K;
+		initData();
+		int result = 0;
+		for (int j = 0; j < K; j++) {
+			int X, Y;
+			cin >> X >> Y;
+			matrix[X][Y] = 1;
+		}
+		for (int j = 0; j < M; j++) {
+			for (int k = 0; k < N; k++) {
+				if (DFS(j, k) == true) {
+					result += 1;
+				}
+			}
+		}
+		cout << result << '\n';
 	}
 }

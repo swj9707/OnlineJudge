c26d57f57d4689e274646e32c6c150454da95328
swj1265 <swj1265@naver.com>
2021-09-25 06:24:10 +0900

    20210925

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/7576.cpp
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/7576.cpp
@@ -16,10 +16,37 @@ void init() {
 	cin.tie(0);
 }
 
+int BFS(int X, int Y) {
+	queue<pair<int, int>> Q;
+	int result = 0;
+	Q.push({ X, Y });
+	while (!Q.empty()) {
+		int x = Q.front().first;
+		int y = Q.front().second;
+		Q.pop();
+		visit[x][y] = true;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && nx < M && ny >= 0 && ny < N && !check[nx][ny] && matrix[nx][ny] == 0) {
+				visit[nx][ny] = true;
+				Q.push({ nx, ny });
+			}
+		}
+		result += 1;
+	}
+	return result;
+}
+
 int main() {
 	cin >> M >> N;
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) cin >> matrix[i][j];
+	}
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			
+		}
 	}
 
 }

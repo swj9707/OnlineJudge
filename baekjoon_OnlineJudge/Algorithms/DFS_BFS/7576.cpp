4c6a24148f823044a8086ba66ec98bd39b16da14
swj1265 <swj1265@naver.com>
2021-09-26 09:29:32 +0900

    20210926

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/7576.cpp
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/7576.cpp
@@ -6,47 +6,59 @@ using namespace std;
 int matrix[MAX][MAX];
 bool visit[MAX][MAX];
 
-int dx = { 1, -1, 0, 0 };
-int dy = { 0, 0, 1, -1 };
-
+int dx[4] = { 1, -1, 0, 0 };
+int dy[4] = { 0, 0, 1, -1 };
 int M, N;
+
+queue<pair<int, int>> Q;
 
 void init() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 }
 
-int BFS(int X, int Y) {
-	queue<pair<int, int>> Q;
-	int result = 0;
-	Q.push({ X, Y });
+void BFS() {
 	while (!Q.empty()) {
-		int x = Q.front().first;
-		int y = Q.front().second;
+		int y = Q.front().first;
+		int x = Q.front().second;
 		Q.pop();
-		visit[x][y] = true;
+		visit[y][x] = true;
 		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
 			int ny = y + dy[i];
-			if (nx >= 0 && nx < M && ny >= 0 && ny < N && !check[nx][ny] && matrix[nx][ny] == 0) {
-				visit[nx][ny] = true;
-				Q.push({ nx, ny });
+			if (nx >= 0 && nx < M && ny >= 0 && ny < N && !visit[ny][nx] && matrix[ny][nx] == 0) {
+				visit[ny][nx] = true;
+				matrix[ny][nx] = matrix[y][x] + 1;
+				Q.push({ ny, nx });
 			}
 		}
-		result += 1;
 	}
-	return result;
 }
 
 int main() {
+	init();
 	cin >> M >> N;
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) cin >> matrix[i][j];
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) cin >> matrix[i][j];
 	}
-	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (matrix[i][j] == 1) Q.push({ i, j });
 		}
 	}
-
+	BFS();
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (matrix[i][j] == 0) {
+				cout << "-1\n";
+				return 0;
+			}
+			if (result < matrix[i][j]) {
+				result = matrix[i][j];
+			}
+		}
+	}
+	cout << result - 1 << '\n';
+	return 0;
 }

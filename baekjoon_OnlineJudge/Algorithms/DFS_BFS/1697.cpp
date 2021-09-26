30c4dcfa96cdf565c8d833c70015c3c64c86cacc
swj1265 <swj1265@naver.com>
2021-09-26 16:22:46 +0900

    20210926

--- baekjoon_OnlineJudge/Algorithms/DFS_BFS/1697.cpp
+++ baekjoon_OnlineJudge/Algorithms/DFS_BFS/1697.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <queue>
-
+#define MAX 100000;
 using namespace std;
 int N, K;
 int dx[3];
+int MATRIX[]
 queue<int> q;
-void BFS() {
+int BFS() {
+	int result = 0;
 	while (!q.empty()) {
 		int x = q.front();
 		q.pop();
+
 		dx[0] = x - 1;
 		dx[1] = x + 1;
 		dx[2] = 2 * x;
+		for (int i = 0; i < 3; i++) {
+			if (dx[i] >= 0) {
+
+			}
+		}
 		
 	}
 }

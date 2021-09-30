e1736d5c84aaddba4d175010c08e6b1c152a63eb
swj1265 <swj1265@naver.com>
2021-10-01 00:51:09 +0900

    20211001

--- baekjoon_OnlineJudge/Algorithms/DynamicProgramming/11057.cpp
+++ baekjoon_OnlineJudge/Algorithms/DynamicProgramming/11057.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 void init() {
@@ -8,7 +8,25 @@ void init() {
 }
 
 int main() {
+	init(); 
 	int N;
 	cin >> N;
-
+	vector<int> DP(10, 1);
+	int answer = 0;
+	if (N == 1) {
+		cout << "10" << '\n';
+		return 0;
+	}
+	else {
+		for (int i = 1; i < N; i++) {
+			for (int j = 8; j >= 0; j++) {
+				DP[i] = DP[i - 1] + DP[i];
+			}
+		}
+		for (int i = 0; i < DP.size(); i++) {
+			answer += DP[i] %10007;
+		}
+	}
+	cout << answer % 10007 << '\n';
+	return 0;
 }

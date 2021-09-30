5ae6672c51b6b5dd15aa39c50e2b14ff91f62dc0
swj1265 <swj1265@naver.com>
2021-09-30 14:05:44 +0900

    20210930

--- baekjoon_OnlineJudge/Algorithms/DynamicProgramming/1912.cpp
+++ baekjoon_OnlineJudge/Algorithms/DynamicProgramming/1912.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#define MAX 100001
 using namespace std;
 
 void init() {
@@ -12,7 +12,14 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> data(N);
+	int dp[MAX] = { 0, };
 	for (int i = 0; i < N; i++) {
 		cin >> data[i];
 	}
+	int answer = dp[0];
+	for (int i = 1; i < N; i++) {
+		dp[i] = max(dp[i], dp[i - 1] + data[i]);
+		if (answer < dp[i]) answer = dp[i];
+	}
+	cout << answer << '\n';
 }

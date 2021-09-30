fd542b082073fcc2c0dea9f23e8fb237a3932e3c
swj1265 <swj1265@naver.com>
2021-09-30 17:17:54 +0900

    20210930

--- baekjoon_OnlineJudge/Algorithms/DynamicProgramming/13398.cpp
+++ baekjoon_OnlineJudge/Algorithms/DynamicProgramming/13398.cpp
@@ -14,27 +14,6 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> data(N);
-	vector<int> dp(N, 0);
-	for (int i = 0; i < N; i++) {
-		cin >> data[i];
-		dp[i] = data[i];
-	}
-	int min_index = min_element(data.begin(), data.end()) - data.begin();
-	int answer1 = dp[0];
-	for (int i = 1; i < data.size(); i++) {
-		dp[i] = max(dp[i], dp[i - 1] + data[i]);
-		if (answer1 < dp[i]) answer1 = dp[i];
-	}
-	data.erase(data.begin() + min_index);
-	vector<int> dp2(N, 0);
-	for (int i = 0; i < data.size(); i++) {
-		dp2[i] = data[i];
-	}
-	int answer2 = dp2[0];
-	for (int i = 1; i < data.size(); i++) {
-		dp2[i] = max(dp2[i], dp2[i - 1] + data[i]);
-		if (answer2 < dp2[i]) answer2 = dp2[i];
-	}
-	int answer = max(answer1, answer2);
-	cout << answer << '\n';
+	vector<int> DP()
+
 }

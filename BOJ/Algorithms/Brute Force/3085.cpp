71d23b5ef539188322f86819ad386632d79cb2b7
swj1265 <swj1265@naver.com>
2021-10-06 19:58:09 +0900

    20211006

--- BOJ/Algorithms/Brute Force/3085.cpp
+++ BOJ/Algorithms/Brute Force/3085.cpp
@@ -1,39 +1,59 @@
 #include <iostream>
 #include <algorithm>
+#define MAX 50
 using namespace std;
 
-void init() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-}
+char data[MAX][MAX];
+int Answer, N;
 
-void Horizontal(char ** board, int N) {
-	for (int i = 0; i < N; i++) {
-		int count = 1;
-		for (int j = 0; j < N; j++) {
-			if (board[i][j] == arr[i][j + 1]) count++;
-			else {
-				if()
+void check() {
+	char c;
+	for (int i = 0; i < N; i++) { 
+		int count = 1; 
+		c = data[i][0];
+		for (int j = 1; j < N; j++) {
+			if ((data[i][j] == c)) {
+				count += 1;
 			}
+			else count = 1;
+			c = data[i][j];
+			if (count > Answer) Answer = count;
+		}
+	}
+
+	for (int j = 0; j < N; j++) { // ¿­ °è»ê
+		int count = 1;
+		c = data[0][j];
+		for (int i = 1; i < N; i++) {
+			if ((data[i][j] == c)) {
+				count += 1;
+			}
+			else count = 1;
+			c = data[i][j];
+			if (count > Answer) Answer = count;
 		}
 	}
 }
+
 
 int main() {
-	int N;
 	cin >> N;
-	char board[51][51];
-
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> board[i][j];
+		for (int j = 0; j < N; j++) cin >> data[i][j];
+	}	
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N-1; j++) {
+			swap(data[i][j], data[i][j + 1]);
+			check();
+			swap(data[i][j], data[i][j + 1]);
+		}
+	}	
+	for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N-1; i++) {
+			swap(data[i][j], data[i+1][j]);
+			check();
+			swap(data[i][j], data[i+1][j]);
 		}
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N - 1; j++) {
-			swap(board[i][j], board[i][j + 1]);
-
-		}
-	}
-
+	cout << Answer << '\n';
 }

4f1e01b57c4e9766023aff7ff9fbe96a35e0356d
swj1265 <swj1265@naver.com>
2021-10-07 00:56:29 +0900

    20211006

--- BOJ/Algorithms/DataStructure/17413.cpp
+++ BOJ/Algorithms/DataStructure/17413.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
     string S;
-    cin >> S;
+    getline(cin, S);
     for(int i = S.length()-1; i >= 0; i--){
         cout << S[i];
     }

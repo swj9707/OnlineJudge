afa145e3f962e104582e7bb74acdf0110036f147
swj1265 <swj1265@naver.com>
2021-10-07 19:49:10 +0900

    20211007

--- BOJ/CodingTestPractice/12865.py
+++ BOJ/CodingTestPractice/12865.py
@@ -11,8 +11,8 @@ dp = [[0]* (K+1) for _ in range(N+1)]
 
 for i in range(N+1):
     for j in range(1, N+1):
-        if j >= item[i][0]:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-itemList[i][0]] + item[i][1])
+        if j >= itemList[i][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-itemList[i][0]] + itemList[i][1])
         else:
             dp[i][j] = dp[i-1][j]
 

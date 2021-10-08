c696dd74bf3043e1d313ee0a78e811962b872cf5
swj1265 <swj1265@naver.com>
2021-10-08 22:44:57 +0900

    20211008

---
+++ Programmers/2021_WeeklyChallenge/2W.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+char returnGrade(int avg){
+    if(avg >= 90) return 'A';
+    else if (avg >= 80 && avg < 90) return 'B';
+    else if (avg >= 70 && avg < 80) return 'C';
+    else if (avg >= 50 && avg < 70) return 'D';
+    else return 'F';
+}
+
+string solution(vector<vector <int>> scores){
+    string answer = "";
+    vector<int> data;
+    int score = 0;
+    for(int i = 0; i < )
+    return answer;
+}

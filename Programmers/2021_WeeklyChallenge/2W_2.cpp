a159eb78d38b5a35c5592857c08fca30b18d2ba2
swj1265 <swj1265@naver.com>
2021-10-08 23:02:41 +0900

    20211008

---
+++ Programmers/2021_WeeklyChallenge/2W_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+string returnGrade(int avg){
+    if(avg >= 90) return "A";
+    else if (avg >= 80 && avg < 90) return "B";
+    else if (avg >= 70 && avg < 80) return "C";
+    else if (avg >= 50 && avg < 70) return "D";
+    else return "F";
+}
+
+string solution(vector<vector <int>> scores){
+    string answer = "";
+    for(int i = 0; i < scores.size(); i++){
+        vector<int> data;
+        int score = 0;
+        for(int j = 0; j < scores[i].size(); j++) data.push_back(scores[j][i]);
+        int max = *max_element(data.begin(), data.end());
+        int min = *min_element(data.begin(), data.end());
+        int maxCount = count(data.begin(), data.end(), max);
+        int minCount = count(data.begin(), data.end(), min);
+        if(data[i] == max){
+            if(maxCount == 1) data.erase();
+        }
+    }
+}

a21042772abee6e32f4cd5ff3afc38def1896d0e
swj1265 <swj1265@naver.com>
2021-10-10 02:40:30 +0900

    20211010

---
+++ BOJ/Algorithms/DFS_BFS/2606.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define MAX 101
+using namespace std;
+
+int matrix[MAX][MAX];
+
+int main(){
+    int N, M;
+    cin >> N;
+    cin >> M;
+    for(int i = 0; i < M; i++){
+
+    }
+}

e98b3b705277288e901baaa3a70eed25485ff1be
swj1265 <swj1265@naver.com>
2021-10-10 21:55:44 +0900

    20211010

--- BOJ/Algorithms/DFS_BFS/2251.cpp
+++ BOJ/Algorithms/DFS_BFS/2251.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
 int A, B, C;
+
+void init(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
 int main(){
+    init();
     cin >> A >> B >> C;
-    
+
 }

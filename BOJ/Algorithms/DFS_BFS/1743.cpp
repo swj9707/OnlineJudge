f02adbb6643600c4b9216a08dbb33333e7ea0072
swj1265 <swj1265@naver.com>
2021-10-10 21:27:13 +0900

    20211010

--- BOJ/Algorithms/DFS_BFS/1743.cpp
+++ BOJ/Algorithms/DFS_BFS/1743.cpp
@@ -2,10 +2,16 @@
 #define MAX 101
 using namespace std;
 int N, M, K;
-    
+int Matrix[MAX][MAX];
+bool visit[MAX];
+
 int main(){
     cin >> N >> M >> K;
     for(int i = 0; i < K; i++){
         int r, c;
+        cin >> r >> c;
+        Matrix[r][c] = 1;
+        Matrix[c][r] = 1;
     }
+    
 }

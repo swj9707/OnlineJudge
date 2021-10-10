c7b03ef701c3eaebed0c633903ddd3f43b529bea
swj1265 <swj1265@naver.com>
2021-10-10 21:33:42 +0900

    20211010

--- BOJ/Algorithms/DFS_BFS/1743.cpp
+++ BOJ/Algorithms/DFS_BFS/1743.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <deque>
+
 #define MAX 101
 using namespace std;
 int N, M, K;
 int Matrix[MAX][MAX];
-bool visit[MAX];
+bool visit[MAX][MAX];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int BFS(int X, int Y){
+    deque<pair<int, int>> Q;
+    Q.push_back({X, Y});
+    visit[X][Y] = true;
+    while(!Q.empty()){
+        int x = Q.front().first;
+        int y = Q.front().second;
+
+    }
+}
 
 int main(){
     cin >> N >> M >> K;
@@ -11,7 +26,5 @@ int main(){
         int r, c;
         cin >> r >> c;
         Matrix[r][c] = 1;
-        Matrix[c][r] = 1;
     }
-    
 }

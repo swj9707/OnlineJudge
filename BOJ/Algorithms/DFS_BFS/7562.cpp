e784fa699cf32981885a6d6f743c4309915bdb8c
swj1265 <swj1265@naver.com>
2021-10-13 12:07:17 +0900

    20211013

--- BOJ/Algorithms/DFS_BFS/7562.cpp
+++ BOJ/Algorithms/DFS_BFS/7562.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <cstring>
+#include <deque>
 #define MAX 301
 
 using namespace std;
 
-int main(){
+int MATRIX[MAX][MAX];
+int N;
+int sourceX, sourceY;
+int destX, destY;
 
+void initSet() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+    cout.tie(0);
+}
+
+void BFS(){
+
+}
+
+int main(){
+	initSet();
+	int T;
+	cin >> T;
+	for(int i = 0; i < T; i++){
+		memset(MATRIX, 0, sizeof(MATRIX));
+
+	}
 }

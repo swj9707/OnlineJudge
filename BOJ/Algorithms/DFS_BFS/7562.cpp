2b3f4375ac88e7c726f475c14fb5277a458f51a6
swj1265 <swj1265@naver.com>
2021-10-14 23:47:01 +0900

    20211014

--- BOJ/Algorithms/DFS_BFS/7562.cpp
+++ BOJ/Algorithms/DFS_BFS/7562.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
 #include <cstring>
-#include <deque>
+#include <queue>
 #define MAX 301
 
 using namespace std;
+
+int MATRIX[MAX][MAX];
+bool Visit[MAX][MAX];
+
+int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+int dy[8] = {2, 1, -1, -2, -2, -1, 1, 2};
 
 int N;
 int sourceX, sourceY;
 int destX, destY;
 
-int dx[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-int dy[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+queue<pair<int, int>> Q;
 
-void initSet() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-    cout.tie(0);
+void initData(){
+	memset(Visit, false, sizeof(Visit));
+	memset(MATRIX, 0, sizeof(MATRIX));
+	while(!Q.empty()) Q.pop();
 }
 
-int BFS(int X, int Y, bool ** Visit){
-	int result = 0;
-	deque<pair<int, int>> Q;
-	Q.push_back({X, Y});
+void BFS(int X, int Y){
+	Q.push({X, Y});
 	Visit[X][Y] = true;
 	while (!Q.empty()){
 		int x = Q.front().first;
 		int y = Q.front().second;
-		Q.pop_front();
+		Q.pop();
 		if(x == destX && y == destY){
-			return result;
+			cout << MATRIX[x][y] << "\n";
+			return;
 		}
 		for(int i = 0; i < 8; i++){
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 			if(nx >= 0 && nx < N && ny >= 0 && ny < N && !Visit[nx][ny]){
 				Visit[nx][ny] == true;
-				result += 1;
-				Q.push_back({nx, ny});
+				MATRIX[nx][ny] = MATRIX[x][y] + 1;
+				Q.push({nx, ny});
 			}
 		}
 	}
@@ -44,19 +48,13 @@ int BFS(int X, int Y, bool ** Visit){
 
 
 int main(){
-	initSet();
 	int T;
 	cin >> T;
 	for(int i = 0; i < T; i++){
+		initData();
 		cin >> N;
-		bool ** VISIT;
-		VISIT = new bool * [N];
-		for(int j = 0; j < N; j++) VISIT[j] = new bool[N];
 		cin >> sourceX >> sourceY;		
 		cin >> destX >> destY;
-		int result = BFS(sourceX, sourceY, VISIT);
-		cout << result << '\n';
-		for(int j = 0; j < N; j++) delete [] VISIT[j];
-		delete [] VISIT;
+		BFS(sourceX, sourceY);
 	}
 }

c36cc8c92a2d5c30d465977c662111fa66f76c4d
swj1265 <swj1265@naver.com>
2021-10-14 22:58:15 +0900

    20211014

--- BOJ/Algorithms/DFS_BFS/11403.cpp
+++ BOJ/Algorithms/DFS_BFS/11403.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include <deque>
-
+#include <vector>
+#include <cstring>
+#define MAX 101
 using namespace std;
 
-int N;
+bool visit[MAX];
+vector <int> v[MAX];
+
+void DFS(int x){
+    for(int i = 0; i < v[x].size(); i++){
+        if(!visit[v[x][i]]){
+            visit[v[x][i]] = true;
+            DFS(v[x][i]);
+        }
+    }
+}
 
 int main(){
+    int N;
     cin >> N;
-        
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            int data;
+            cin >> data;
+            if(data == 1)v[i].push_back(j);
+        }
+    }
+    for(int i = 0; i < N; i++){
+        memset(visit, false, sizeof(visit));
+        DFS(i);
+        for(int j = 0 ; j < N; j++){
+            cout << visit[j] << " ";
+        }
+        cout << "\n";
+    }
 }

225879abe4288b2773f8e841a581776dd4ebb23a
swj1265 <swj1265@naver.com>
2021-10-18 21:11:17 +0900

    20211018

---
+++ BOJ/Algorithms/DFS_BFS/9372.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#define MAX 1001
+
+using namespace std;
+
+int N, M;
+
+int main(){
+    int T;
+    cin >> T;
+    for(int i = 0; i < T; i++){
+        cin >> N >> M;
+    }
+
+}

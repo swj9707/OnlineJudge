465f59e51a20b7a58968392bf9e988a3a78e9c2d
swj1265 <swj1265@naver.com>
2021-10-19 21:51:45 +0900

    20211019

--- BOJ/Algorithms/DFS_BFS/5639.cpp
+++ BOJ/Algorithms/DFS_BFS/5639.cpp
@@ -3,27 +3,34 @@
 using namespace std;
 
 typedef struct Node{
-    int data;
+    int key;
     Node * left;
     Node * right;
 } Node;
 
-Node * insert(Node * node, int data){
+Node * insert(Node * node, int key){
     if(node == NULL){
         node = new Node();
-        node -> data = data;
+        node -> key = key;
         node -> left = node -> right = NULL;
     }
-    else if(data <= node -> data) node -> left = insert(node->left, data);
-    else node -> right = insert(node->right, data);
+    else if(key <= node -> key) node -> left = insert(node->left, key);
+    else node -> right = insert(node->right, key);
     return node;
 }
 
+void postorder(Node * node){
+    if(node -> left != NULL) postorder(node->left);
+    if(node-> right != NULL) postorder(node -> right);
+    cout << node -> key << '\n';
+}
 
 int main(){
     int val;
+    Node * Root = NULL;
     while(cin >> val){
         if(val == EOF) break;
+        Root = insert(Root, val);
     }
-
+    postorder(Root);
 }

26902bdac98808ed46c3c3ea6743e18497b6b968
swj1265 <swj1265@naver.com>
2021-10-19 19:29:08 +0900

    20211019

--- BOJ/Algorithms/DFS_BFS/5639.cpp
+++ BOJ/Algorithms/DFS_BFS/5639.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+#define MAX 10001
 using namespace std;
 
+typedef struct Node{
+    int data;
+    Node * left;
+    Node * right;
+} Node;
+
 int main(){
+    int val;
+    while(cin >> val){
+        if(val == EOF) break;
+    }
 
 }

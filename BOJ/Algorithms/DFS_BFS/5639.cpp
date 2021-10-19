576ff3f1ee781e0116663bd3216275b61b876052
swj1265 <swj1265@naver.com>
2021-10-19 20:17:22 +0900

    20211019

--- BOJ/Algorithms/DFS_BFS/5639.cpp
+++ BOJ/Algorithms/DFS_BFS/5639.cpp
@@ -8,6 +8,18 @@ typedef struct Node{
     Node * right;
 } Node;
 
+Node * insert(Node * node, int data){
+    if(node == NULL){
+        node = new Node();
+        node -> data = data;
+        node -> left = node -> right = NULL;
+    }
+    else if(data <= node -> data) node -> left = insert(node->left, data);
+    else node -> right = insert(node->right, data);
+    return node;
+}
+
+
 int main(){
     int val;
     while(cin >> val){

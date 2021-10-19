5793aa566f6a67f85a3bdba4b4bad1b6b4d8eb60
swj1265 <swj1265@naver.com>
2021-10-19 23:25:17 +0900

    20211019

--- Programmers/CodingTestPractice/Hash/42576.cpp
+++ Programmers/CodingTestPractice/Hash/42576.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include <string>
+#include <vector>
 #include <unordered_map>
-
 using namespace std;
 
-int main(){
-    
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    unordered_map<string, int> d;
+    for(auto& i : participant) d[i]++;
+    for(auto& i : completion) d[i]--;
+    for(auto& i : d){
+        if(i.second > 0){
+            answer = i.first;
+            break;
+        }
+    }
+    return answer;
 }

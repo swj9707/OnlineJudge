99e0d4903f96fa2c2b70d9e50b5b6d157e4bcff5
swj1265 <swj1265@naver.com>
2021-10-19 16:39:33 +0900

    20211019

--- BOJ/Algorithms/DFS_BFS/7569.cpp
+++ BOJ/Algorithms/DFS_BFS/7569.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <deque>
 #define MAX 101
+
 using namespace std;
 
 typedef struct point{
     int Y;
     int X;
-    int Z;
+    int H;
 } point;
 
-int dx[6] = {0, 0, 1, -1, 0, 0};
-int dy[6] = {1, -1, 0, 0, 0, 0};
-int dz[6] = {0, 0, 0, 0, 1, -1};
-
+int M, N, H;
 int MATRIX[MAX][MAX][MAX];
 bool visit[MAX][MAX][MAX];
-int M, N, H;
-deque<point> Q;
+deque<point> DQ;
 
 void initSet() {
 	ios_base::sync_with_stdio(0);
@@ -24,52 +21,6 @@ void initSet() {
     cout.tie(0);
 }
 
-void BFS(){
-    while(!Q.empty()){
-        int Y = Q.front().Y;
-        int X = Q.front().X;
-        int Z = Q.front().Z;
-        Q.pop_front();
-        visit[Y][X][Z] = true;
-        for(int i = 0; i < 6; i++){
-            int nx = dx[i];
-            int ny = dy[i];
-            int nz = dz[i];
-            if(ny >= 0 && ny < N && nx >= 0 && nx < M && nz >= 0 && nz < H){
-                visit[ny][nx][nz] = true;
-                MATRIX[ny][nx][nz] = MATRIX[Y][X][Z] + 1;
-                Q.push_back({ny, nx, nz});
-            }
-        }
-    }
-}
-
 int main(){
-    initSet();
-    cin >> M >> N >> H;
-    for(int y = 0; y < N; y++){
-        for(int x = 0; x < M; x++){
-            for(int h = 0; h < H; h++){
-                cin >> MATRIX[y][x][h];
-                if(MATRIX[y][x][h] == 1){
-                    Q.push_back({y, x, h});
-                    visit[y][x][h] = true;
-                }
-                else if(MATRIX[y][x][h] == -1) visit[y][x][h] = true;
-            }
-        }
-    }
-    if(Q.empty()) cout << "-1\n";
-    else {
-        BFS();
-        int result = 0;
-        for(int y = 0; y < N; y++){
-            for(int x = 0; x < M; x++){
-                for(int h = 0; h < H; h++){
-                    if(result < MATRIX[y][x][h]) result = MATRIX[y][x][h];
-                }
-            }
-        }
-        cout << result -1 << '\n';
-    }
+
 }

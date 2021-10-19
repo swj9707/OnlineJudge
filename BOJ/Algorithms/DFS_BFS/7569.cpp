96a2833e9e6240c6e86aac99008dc772df418f0b
swj1265 <swj1265@naver.com>
2021-10-19 19:04:33 +0900

    20211019

--- BOJ/Algorithms/DFS_BFS/7569.cpp
+++ BOJ/Algorithms/DFS_BFS/7569.cpp
@@ -1,80 +1,84 @@
 #include <iostream>
-#include <deque>
+#include <queue>
 #define MAX 101
 using namespace std;
 
-typedef struct point{
-    int Y;
-    int X;
-    int Z;
-} point;
+typedef struct
+{
+    int x, y, z;
+    int cnt;
+} types;
 
-int dx[6] = {0, 0, 1, -1, 0, 0};
-int dy[6] = {1, -1, 0, 0, 0, 0};
-int dz[6] = {0, 0, 0, 0, 1, -1};
-
-int MATRIX[MAX][MAX][MAX];
-bool visit[MAX][MAX][MAX];
 int M, N, H;
-deque<point> Q;
-
-void initSet() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-    cout.tie(0);
+int MATRIX[MAX][MAX][MAX];
+bool visited[MAX][MAX][MAX];
+int moveX[6] = {1, -1, 0, 0, 0, 0};
+int moveY[6] = {0, 0, 1, -1, 0, 0};
+int moveZ[6] = {0, 0, 0, 0, 1, -1};
+queue<types> bfs;
+void init(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 }
+int BFS()
+{
+    int cnt = 0;
+    while (!bfs.empty())
+    {
+        auto a = bfs.front();
+        bfs.pop();
+        cnt = max(cnt, a.cnt);
+        for (int i = 0; i < 6; i++)
+        {
+            int nx = a.x + moveX[i];
+            int ny = a.y + moveY[i];
+            int nz = a.z + moveZ[i];
 
-void BFS(){
-    while(!Q.empty()){
-        int Y = Q.front().Y;
-        int X = Q.front().X;
-        int Z = Q.front().Z;
-        Q.pop_front();
-        visit[Y][X][Z] = true;
-        for(int i = 0; i < 6; i++){
-            int nx = X + dx[i];
-            int ny = Y + dy[i];
-            int nz = Z + dz[i];
-            if(ny >= 0 && ny < M && nx >= 0 && nx < N && nz >= 0 && nz < H && !visit[ny][nx][nz] && MATRIX[ny][nx][nz] == 0){
-                //cout << ny << " " << nx << " " << nz << '\n';
-                visit[ny][nx][nz] = true;
-                MATRIX[ny][nx][nz] = MATRIX[Y][X][Z] + 1;
-                Q.push_back({ny, nx, nz});
+            if (nx >= 0 && ny >= 0 && nz >= 0 && nx < N && ny < M && nz < H && !visited[nx][ny][nz])
+            {
+                bfs.push({nx, ny, nz, a.cnt + 1});
+                visited[nx][ny][nz] = true;
             }
         }
     }
+    return cnt;
 }
 
-int main(){
-    initSet();
+int main(void)
+{
+    init();
+    int cnt = 0;
     cin >> M >> N >> H;
-    for(int h = 0; h < H; h++){
-        for(int y = 0; y < M; y++){
-            for(int x = 0; x < N; x++){
-                cin >> MATRIX[y][x][h];
-                if(MATRIX[y][x][h] == 1){
-                    Q.push_back({y, x, h});
-                    visit[y][x][h] = true;
+    for (int k = 0; k < H; k++)
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+            {
+                cin >> MATRIX[i][j][k];
+                if (MATRIX[i][j][k] == 1)
+                {
+                    bfs.push({i, j, k, 0});
+                    visited[i][j][k] = true;
                 }
-                else if(MATRIX[y][x][h] == -1) visit[y][x][h] = true;
+                else if (MATRIX[i][j][k] == -1)
+                    visited[i][j][k] = true;
             }
-        }
+    
+    if (bfs.empty())
+    {
+        cout << "-1\n";
+        return 0;
     }
-    if(Q.empty()) cout << "-1\n";
-    else {
-        BFS();
-        int result = 0;
-        for(int h = 0; h < H; h++){
-            for(int y = 0; y < M; y++){
-                for(int x = 0; x < N; x++){
-                    if(MATRIX[y][x][h] == 0){
-                        cout << "-1\n";
-                        return 0;
-                    }
-                    if(result < MATRIX[y][x][h]) result = MATRIX[y][x][h];
+
+    cnt = BFS();
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < M; j++)
+            for (int k = 0; k < H; k++)
+                if (!visited[i][j][k])
+                {            
+                    cout << "-1\n";
+                    return 0;
                 }
-            }
-        }
-        cout << result -1 << '\n';
-    }
+
+    cout << cnt << '\n';
+    return 0;
 }

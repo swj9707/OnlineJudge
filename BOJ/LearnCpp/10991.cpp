be526807de2a3103dd7a483fa7096a3c9dba6d70
swj1265 <swj1265@naver.com>
2021-10-22 23:16:32 +0900

    20211022

---
+++ BOJ/LearnCpp/10991.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+	{
+		for (int j = i; j <= N - 1; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 1; j <= i*2-1; j++)
+		{
+			if (j % 2 == 1)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+}

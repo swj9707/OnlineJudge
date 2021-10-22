109be4c61f0f1e979823682daac17b8aaca4ac81
swj1265 <swj1265@naver.com>
2021-10-22 18:23:23 +0900

    20211022

---
+++ BOJ/Algorithms/Greedy/4796.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int L, P, V;
+    //캠핑장을 연속하는 P일 중 L일 동안만 사용 가능함. 
+    //강산이는 이제 막 V일 짜리 휴가를 시작함
+    while(L != 0 && P != 0 && V != 0){
+        cin >> L >> P >> V;
+        
+    }
+}

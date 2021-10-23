ee18a94a1e0351846bed26437849ac6a61f51c63
swj1265 <swj1265@naver.com>
2021-10-23 19:51:25 +0900

    20211023

--- BOJ/Algorithms/DFS_BFS/2573.cpp
+++ BOJ/Algorithms/DFS_BFS/2573.cpp
@@ -14,29 +14,32 @@ int dy[4] = {0, 0, 1, -1};
 
 int N, M;
 
-void printMatrix(){
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++) cout << MATRIX[i][j] << " ";
+void printMatrix()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+            cout << MATRIX[i][j] << " ";
         cout << "\n";
     }
 }
 
-void test(){
-    V.p
-}
-
-void BFS(int Y, int X){
+void BFS(int Y, int X)
+{
     deque<pair<int, int>> DQ;
     DQ.push_back({Y, X});
     Visit[Y][X] = true;
-    while(!DQ.empty()){
+    while (!DQ.empty())
+    {
         int y = DQ.front().first;
         int x = DQ.front().second;
         DQ.pop_front();
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++)
+        {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if(ny >= 0 && ny < N && nx >= 0 && nx < M && !Visit[ny][nx]){
+            if (ny >= 0 && ny < N && nx >= 0 && nx < M && !Visit[ny][nx])
+            {
                 Visit[ny][nx] = true;
                 DQ.push_back({ny, nx});
             }
@@ -44,41 +47,67 @@ void BFS(int Y, int X){
     }
 }
 
-
-void Melt(){
+void Melt()
+{
     int tmp[MAX][MAX];
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
             tmp[i][j] = MATRIX[i][j];
         }
     }
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            if(tmp[i][j] != 0){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (tmp[i][j] != 0)
+            {
                 int count = 0;
-                for(int k = 0; k < 4; k++){
+                for (int k = 0; k < 4; k++)
+                {
                     int x = j + dx[k];
                     int y = i + dy[k];
-                    if(tmp[y][x] == 0) count += 1;
+                    if (tmp[y][x] == 0)
+                        count += 1;
                 }
                 MATRIX[i][j] -= count;
-                if(MATRIX[i][j] < 0) MATRIX[i][j] = 0;
+                if (MATRIX[i][j] < 0)
+                    MATRIX[i][j] = 0;
             }
         }
     }
 }
 
-int main(){
+int main()
+{
     cin >> N >> M;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+            cin >> MATRIX[i][j];
     }
-    int SearchCount = 0;
-    while(SearchCount < 2){
-    memset(Visit, false, sizeof(Visit));
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
+    int Answer = 0;
+    while (true)
+    {
+        memset(Visit, false, sizeof(Visit));
+        int SearchCount = 0;
+        Melt();
+        printMatrix();
+        Answer += 1;
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < M; j++)
+            {
+                if (!Visit[i][j] && MATRIX[i][j] != 0)
+                {
+                    BFS(i, j);
+                    SearchCount += 1;
+                }
+            }
+        }
+        if(SearchCount > 1) break;
     }
-    }
+    cout << Answer << '\n';
 }

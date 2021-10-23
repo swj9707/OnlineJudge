b011093d182cda905bb85bffaf306ac5016a4695
swj1265 <swj1265@naver.com>
2021-10-23 09:00:55 +0900

    20211023

--- BOJ/Algorithms/DFS_BFS/2573.cpp
+++ BOJ/Algorithms/DFS_BFS/2573.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+#include <cstring>
 #define MAX 300
 using namespace std;
 
 int MATRIX[MAX][MAX];
 bool Visit[MAX][MAX];
-
+vector<int> V;
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
+int N, M;
+
+void printMatrix(){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++) cout << MATRIX[i][j] << " ";
+        cout << "\n";
+    }
+}
+
 int main(){
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
+    }
 
 }

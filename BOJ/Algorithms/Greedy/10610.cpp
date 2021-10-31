c2bab4bc9c5be1a75579524839d508cdb36f577b
swj1265 <swj1265@naver.com>
2021-10-31 16:28:14 +0900

    20211031

--- BOJ/Algorithms/Greedy/10610.cpp
+++ BOJ/Algorithms/Greedy/10610.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-//Maximum = 100000
+//끝자리의 수가 0이며 각 자리의 수의 합이 3의 배수
 int main(){
-
+    string N;
+    cin >> N;
+    vector<int> data;
+    for(int i = 0; i < N.size(); i++){
+        int inputData = N[i] - '0';
+        data.push_back(inputData);
+    }
+    sort(data.begin(), data.end());
+    if(data[0] != 0) {cout << "-1\n"; return 0;}
+    else{
+        int sum = 0;
+        for(int i = 1; i < data.size(); i++){
+            sum += data[i];
+        }
+        if(sum % 3 != 0) {cout << "-1\n"; return 0;}
+        else{
+            string answer = "";
+            for(int i = data.size() - 1; i >= 1; i--) {
+                answer += (char)(data[i]+48);
+            }
+            answer += "0";
+            cout << answer << '\n';
+            return 0;
+        }
+    }
 }

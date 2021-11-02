713602a99494b3568e5b44e7bc8812ace4d6c583
swj1265 <swj1265@naver.com>
2021-11-02 14:21:59 +0900

    20211102

--- BOJ/Algorithms/Greedy/12904.cpp
+++ BOJ/Algorithms/Greedy/12904.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
-
-int main(){
-    string S, T;
-    cin >> S;
-    cin >> T;
-    //역방향
-    //1. A를 뺀다
-    //2. B를 뺴고 문자열을 뒤집는다. 
+bool flag;
+string S, T;
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> S >> T; 
+    while (1)
+    {
+        if (S.length() == T.length())
+        {
+            if (S == T)
+                flag = true;
+            break;
+        }
+        char c = T[T.length() - 1];
+        T.pop_back();
+        if (c == 'B')
+            reverse(T.begin(), T.end());
+    }
+    cout << flag << "\n";
+    return 0;
 }
--- BOJ/Algorithms/Greedy/1783.cpp
+++ BOJ/Algorithms/Greedy/1783.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main(){
     int N, M;
     cin >> N >> M;
-    
+    if (N == 1){
+        cout << "1\n";
+    }
+    else if (N == 2){
+        cout << min(4, (M+1)/2) << "\n";
+    }
+    else if (N >= 3){
+        if(M<=6) cout << min(4, M) << "\n";
+        else cout << M-2 << "\n";
+    }
 }

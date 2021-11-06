15f0df13535e0a789d6977748c236ba50f1aaee4
swj1265 <swj1265@naver.com>
2021-11-06 23:42:45 +0900

    20211106

--- BOJ/Algorithms/DFS_BFS/15558.cpp
+++ BOJ/Algorithms/DFS_BFS/15558.cpp
@@ -7,37 +7,41 @@ int N, K;
 
 void BFS()
 {
-    int count = 1;
-    deque<pair<int, int>> DQ;
-    DQ.push_back({0, 1});
+    deque<pair<pair<int, int>, int>> DQ;
+    DQ.push_back({{0, 1}, 1});
     visit[0][1] = true;
     while (!DQ.empty())
     {
-        int line = DQ.front().first;
-        int index = DQ.front().second;
+        int line = DQ.front().first.first;
+        int index = DQ.front().first.first;
+        int count = DQ.front().second;
         DQ.pop_front();
         if (index > N)
         {
             cout << "1\n";
             return;
         }
+        if(count >= N) break;
         if(!visit[line][index+1]){
-            DQ.push_back({line, index + 1});
+            DQ.push_back({{line, index + 1}, count + 1});
             visit[line][index + 1] = true;
+            visit[0][count] = true;
+            visit[1][count] = true;
         }
         if(index - 1 > 0 && !visit[line][index-1]){
-            DQ.push_back({line, index - 1});
+            DQ.push_back({{line, index - 1}, count + 1});
             visit[line][index - 1] = true;
+            visit[0][count] = true;
+            visit[1][count] = true;
         }
         if(line == 0) line = 1;
         else line = 0;
         if(!visit[line][index + K]){
-            DQ.push_back({line, index + K});
+            DQ.push_back({{line, index + K}, count + 1});
             visit[line][index + K] = true;
+            visit[0][count] = true;
+            visit[1][count] = true;
         }
-        visit[0][count] = true;
-        visit[1][count] = true;
-        count++;
     }
     cout << "0\n";
     return;

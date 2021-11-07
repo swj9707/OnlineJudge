43dd8b83593bc200313d4b8337fe10d51201ee1a
swj1265 <swj1265@naver.com>
2021-11-07 21:01:07 +0900

    20211107

--- Programmers/CodingTestPractice/Greedy/42885.cpp
+++ Programmers/CodingTestPractice/Greedy/42885.cpp
@@ -1,16 +1,47 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> people, int limit){
-    int answer = people.size();
+bool desc(int a, int b)
+{
+    return a > b;
 }
 
-int main(){
-    vector<int> people1 = {70,50,80,50};
-    vector<int> people2 = {70,80,50};
+int solution(vector<int> people, int limit)
+{
+    int answer = 0;
+    sort(people.begin(), people.end(), desc);
+    while (!people.empty())
+    {
+        if (people.size() > 1)
+        {
+            if (people[0] + people[people.size() - 1] <= 100)
+            {
+                answer++;
+                people.pop_back();
+                people.erase(people.begin());
+            }
+            else
+            {
+                answer++;
+                people.erase(people.begin());
+            }
+        }
+        else{
+            answer++;
+            people.erase(people.begin());
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<int> people1 = {70, 50, 80, 50};
+    vector<int> people2 = {70, 80, 50};
     cout << solution(people1, 100) << '\n';
     cout << solution(people2, 100) << '\n';
 }

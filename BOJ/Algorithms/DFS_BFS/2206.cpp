e299929acd19d9b4e654b2787baf03a0f43648a6
swj1265 <swj1265@naver.com>
2021-11-08 18:01:25 +0900

    20211108

--- BOJ/Algorithms/DFS_BFS/2206.cpp
+++ BOJ/Algorithms/DFS_BFS/2206.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <deque>
-#define MAX 1001
+#include <tuple>
+#define MAX 1000
 
 using namespace std;
 
 int N, M;
 int MATRIX[MAX][MAX];
-bool visit[MAX][MAX];
+int visit[MAX][MAX][2];
 
-int BFS(){
-    deque<pair<int, int>> Q;
-    Q.push_back({})
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+void BFS(){
+    deque<tuple<int, int, int>> Q;
+    Q.push_back({0, 0, 1});
+    visit[0][0][1];
+    while(!Q.empty()){
+        int X , Y, block;
+        tie(X, Y, block) = Q.front();
+        Q.pop_front();
+        if(X == N - 1 && Y == M - 1){
+            cout << visit[X][Y][block] << "\n";
+            return;
+        }
+        for(int i = 0; i < 4; i++){
+            int nx = X + dx[i];
+            int ny = Y + dy[i];
+            if(nx >= 0 && )
+        }
+    }
 }
 
 int main(){
@@ -18,5 +37,5 @@ int main(){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
     }
-
+    BFS();
 }

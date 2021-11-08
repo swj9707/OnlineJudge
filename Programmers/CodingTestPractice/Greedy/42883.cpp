4b679b5a42d34b7dcef29781e14a3e5cfd42c595
swj1265 <swj1265@naver.com>
2021-11-09 00:08:50 +0900

    20211109

---
+++ Programmers/CodingTestPractice/Greedy/42883.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+string solution(string number, int k){
+    string answer = "";
+    int maxSize = number.size() - k;
+    int start = 0;
+    return answer;
+}
+
+int main(){
+    cout << solution("1924", 2) << "\n";
+    cout << solution("1231234", 3) << "\n";
+    cout << solution("4177252841", 4) << "\n";
+}

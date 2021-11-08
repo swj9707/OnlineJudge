73991dc28ad2e737f4d1cbacd53af7f10a69e58b
swj1265 <swj1265@naver.com>
2021-11-08 18:23:29 +0900

    20211108

--- BOJ/Algorithms/DFS_BFS/2206.cpp
+++ BOJ/Algorithms/DFS_BFS/2206.cpp
@@ -20,6 +20,7 @@ void BFS(){
         int X , Y, block;
         tie(X, Y, block) = Q.front();
         Q.pop_front();
+        cout << X << " " << Y << " " << block << '\n';
         if(X == N - 1 && Y == M - 1){
             cout << visit[X][Y][block] << "\n";
             return;
@@ -27,15 +28,27 @@ void BFS(){
         for(int i = 0; i < 4; i++){
             int nx = X + dx[i];
             int ny = Y + dy[i];
-            if(nx >= 0 && )
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M){
+                if(MATRIX[nx][ny] == 1 && block == 1){
+                    visit[nx][ny][block - 1] = visit[X][Y][block] + 1;
+                    Q.push_back({nx, ny, block - 1});
+                }
+                if(MATRIX[nx][ny] == 1 && visit[nx][ny][block] == 0){
+                    visit[nx][ny][block] = visit[nx][ny][block] + 1;
+                    Q.push_back({nx, ny, block});
+                }
+            }
         }
     }
+    cout << "-1\n";
+    return;
 }
 
 int main(){
     cin >> N >> M;
     for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
+        string tmp; cin >> tmp;
+        for(int j = 0; j < M; j++) MATRIX[i][j] = tmp[j] - '0';
     }
     BFS();
 }

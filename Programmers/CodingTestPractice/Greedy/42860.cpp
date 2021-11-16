7f04866dffe3fffc22c89acd6dd52bec3e139d87
swj1265 <swj1265@naver.com>
2021-11-16 20:54:25 +0900

    20211116

--- Programmers/CodingTestPractice/Greedy/42860.cpp
+++ Programmers/CodingTestPractice/Greedy/42860.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 int solution(string name) {
     int answer = 0;
+    for(int i = 0; i < name.size(); i++){
+
+        cout << name[i] << " " << int(name[i]) - 65 << " ";
+    }
+    cout << '\n';
     return answer;
+}
+
+int main(){
+    cout << solution("JEROEN") << '\n';
+    cout << solution("JAN") << '\n';
 }

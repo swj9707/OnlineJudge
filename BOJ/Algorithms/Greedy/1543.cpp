778449ea49d5a57ed2502caa09fe5fe26422ee8e
swj1265 <swj1265@naver.com>
2021-11-17 09:17:25 +0900

    20211117

--- BOJ/Algorithms/Greedy/1543.cpp
+++ BOJ/Algorithms/Greedy/1543.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main(){
     string tmp, find;
-    cin >> tmp;
-    cin >> find;
+    getline(cin, tmp);
+    getline(cin, find);
+    int answer = 0;
+    if(find.length() > tmp.length()) {
+        cout << "0\n"; return 0;
+    }
+    else{
+        for(int i = 0; i < tmp.length(); i++){
+            bool flag = true;
+            for(int j = 0; j < find.length(); j++){
+                if(tmp[i+j] != find[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                answer++;
+                i += find.length()-1;
+            }
+        }
+    }
+    cout << answer;
 }

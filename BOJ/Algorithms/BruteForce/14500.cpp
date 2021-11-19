420e3df474f14c7d28a754082462216fe64f0546
swj1265 <swj1265@naver.com>
2021-11-19 14:40:15 +0900

    20211119

---
+++ BOJ/Algorithms/BruteForce/14500.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#define MAX 500
+using namespace std;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int N, M;
+int MATRIX[MAX][MAX];
+bool Visit[MAX][MAX];
+
+void DFS(int x, in)
+
+int main(){
+    cin >> N >> M;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> MATRIX[i][j];
+        }
+    }
+}

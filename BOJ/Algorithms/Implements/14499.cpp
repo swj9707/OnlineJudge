6ac721768f2b35d722ecaaf672c47cf68c8bf56a
swj1265 <swj1265@naver.com>
2021-11-22 15:13:05 +0900

    20211122

--- BOJ/Algorithms/Implements/14499.cpp
+++ BOJ/Algorithms/Implements/14499.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-
+#define DICE 6
 #define MAX 21
 
 using namespace std;
 
-int N, M, x, y, K;
+int N, M, initx, inity, K;
+//이동 방향 : 동 1 서 2 북 3 남 4
 int MATRIX[MAX][MAX];
+int dice[DICE] = {}
+
+int roll(int movepoint){
+    return 0;
+}
 
 int main(){
-    cin >> N >> M >> x >> y >> K;
+    cin >> N >> M >> initx >> inity >> K;
+    //가로 세로 크기, 주사위를 놓은 곳, 명령의 갯수
     for(int i = 0; i < N; i++){
-
+        for(int j = 0; j < M; j++) cin >> MATRIX[i][j];
+    }
+    int movepoint;
+    for(int i = 0; i < K; i++){
+        cin >> movepoint;
     }
 }

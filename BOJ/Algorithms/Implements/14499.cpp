4a5a008a7a838a00a48b803e9a5288cbdd97b6c7
swj1265 <swj1265@naver.com>
2021-11-22 13:59:40 +0900

    20211122

--- BOJ/Algorithms/Implements/14499.cpp
+++ BOJ/Algorithms/Implements/14499.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#define MAX 21
+
 using namespace std;
 
+int N, M, x, y, K;
+int MATRIX[MAX][MAX];
+
 int main(){
-    
+    cin >> N >> M >> x >> y >> K;
+    for(int i = 0; i < N; i++){
+
+    }
 }

82c9cdf7044362eea8f9c57730a316c99315373e
swj1265 <swj1265@naver.com>
2021-11-27 23:19:10 +0900

    20211127

---
+++ BOJ/Algorithms/Greedy/2847.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int N;
+    int answer = 0;
+    cin >> N;
+    vector<int> vec(N);
+    /*
+    5 3 7 5 -> 2(3) 3 4(3) 5 
+    */
+    for(int i = N - 1; i >= 0; i--){
+    }
+}

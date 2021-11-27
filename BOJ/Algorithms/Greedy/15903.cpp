7f89a602c430be846baa5d62efea47d0037ca439
swj1265 <swj1265@naver.com>
2021-11-27 23:42:56 +0900

    20211127

--- BOJ/Algorithms/Greedy/15903.cpp
+++ BOJ/Algorithms/Greedy/15903.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 using namespace std;
 
 int main(){
     int n, m;
     cin >> n >> m;
-        
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    long long answer = 0;
+    for(int i =0; i < n; i++){
+        int data; cin >> data; pq.push(data);
+    }
+    for(int i = 0; i < m; i++){
+        long long a = pq.top();
+        pq.pop();
+        long long b = pq.top();
+        pq.pop();
+        pq.push(a+b);
+        pq.push(a+b);
+    }
+    for(int i = 0 ; i < n; i++){
+        answer += pq.top();
+        pq.pop();
+    }
+    cout << answer << '\n';
 }

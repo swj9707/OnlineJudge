1ccd7488839a86b3ba32a354679d2f9a581d9da9
swj1265 <swj1265@naver.com>
2021-12-02 20:13:39 +0900

    20211202

--- BOJ/Algorithms/Greedy/9237.cpp
+++ BOJ/Algorithms/Greedy/9237.cpp
@@ -7,7 +7,11 @@ int main(){
     int N;
     cin >> N;
     vector<int> vec(N);
+    int answer = 0;
     for(int i = 0; i < N; i++) cin >> vec[i];
     sort(vec.begin(), vec.end());
-
+    for(int i = 0; i < N; i++){
+        answer = max(answer, vec[i] + i + 1);
+    }
+    cout << answer + 1 << '\n';
 }

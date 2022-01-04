5d99670d2933e48572a2c360a3abd59e5a639471
swj1265 <swj1265@naver.com>
2022-01-04 17:26:01 +0900

    20220104

--- BOJ/Algorithms/TwoPointer/3273.cpp
+++ BOJ/Algorithms/TwoPointer/3273.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 int Arr[MAX];
 int main(){
-    int N, int X;
+    int N, X;
     cin >> N;
-    for(int i = 1; i < N; i++) cin >> Arr[i];
+    for(int i = 1; i <= N; i++) cin >> Arr[i];
     cin >> X;
-
+    int result = 0, count = 0;
+    int start = 0, end = 1;
+    while(start < end && end < N){
+        result = Arr[start] + Arr[end];
+        cout << start << " " << end << '\n';
+        if(result == X){
+            count++;
+            start++;
+            end = start + 1;
+        }
+        else end++;
+    }
+    cout << count << '\n';
 }

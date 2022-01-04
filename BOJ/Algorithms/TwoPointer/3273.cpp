1c8d0676ad3533f2790ec80a2b19fa2e589c2af7
swj1265 <swj1265@naver.com>
2022-01-04 17:17:43 +0900

    20220104

---
+++ BOJ/Algorithms/TwoPointer/3273.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define MAX 100001
+using namespace std;
+int Arr[MAX];
+int main(){
+    int N, int X;
+    cin >> N;
+    for(int i = 1; i < N; i++) cin >> Arr[i];
+    cin >> X;
+
+}

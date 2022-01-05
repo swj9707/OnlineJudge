907688d3a2f2325edc913d9df705e87b6d8537e5
swj1265 <swj1265@naver.com>
2022-01-05 23:15:06 +0900

    20220105

--- BOJ/Algorithms/Tree/1991.cpp
+++ BOJ/Algorithms/Tree/1991.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 
 using namespace std;
-typedef struct _node{
-    _node * left;
-    _node * right;
-    char value;
-} node;
 
-void preorder(node * n){
-    cout << n->value;
-    if(n->left != NULL) preorder(n->left);
-    if(n->right != NULL) preorder(n->right);
+pair<int, int> node[26];
+
+void preorder(char current){
+    if(current == '.') return;
+    cout << current;
+    preorder(node[current - 'A'].first);
+    preorder(node[current - 'A'].second);
 }
-void inorder(node * n){
-    if(n->left != NULL) inorder(n->left);
-    cout << n-> value;
-    if(n->right != NULL) inorder(n->right);
+
+void inorder(char current){
+    if(current == '.') return;
+    inorder(node[current - 'A'].first);
+    cout << current;
+    inorder(node[current - 'A'].second);
 }
-void postorder(node *n){
-    if(n->left != NULL) postorder(n->left);
-    if(n->right != NULL) postorder(n->right);
-    cout << n->value;
+
+void postorder(char current){
+    if(current == '.') return;
+    postorder(node[current - 'A'].first);
+    postorder(node[current - 'A'].second);
+    cout << current;
 }
 
 int main(){
-
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        char ch, left, right;
+        cin >> ch >> left >> right;
+        node[ch - 'A'] = {left, right};
+    }
+    preorder('A');
+    cout << '\n';
+    inorder('A');
+    cout << '\n';
+    postorder('A');
+    cout << '\n';
 }

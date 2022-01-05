c170335b920142c2c2add3ca95dc155d97c60864
swj1265 <swj1265@naver.com>
2022-01-06 00:49:19 +0900

    20220106

--- BOJ/Algorithms/Stack/10799.cpp
+++ BOJ/Algorithms/Stack/10799.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main(){
-
+    string tmp;
+    stack<char> stk;
+    int answer = 0;
+    cin >> tmp;
+    for(int i = 0; i < tmp.size(); i++){
+        if(tmp[i] == '(') stk.push(i);
+        else{
+            if(tmp[i - 1] == '('){//레이저다?
+                stk.pop();
+                answer += stk.size();
+            } 
+            else{//파이프가 끊켰다?
+                answer += 1;
+                stk.pop();
+            }
+        }
+    }
+    cout << answer << '\n';
 }

febad771fa4e30316b51c1e07a3a83def0f97080
swj1265 <swj1265@naver.com>
2022-01-06 16:48:14 +0900

    20220106

--- BOJ/Algorithms/NumberTheory/1735.cpp
+++ BOJ/Algorithms/NumberTheory/1735.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
-int main(){
-    
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int main()
+{
+    int A1, B1, A2, B2;
+    cin >> A1 >> B1;
+    cin >> A2 >> B2;
+    int C1, C2;
+    C2 = B1 * B2;
+    C1 = A1 * B2 + A2 * B1;
+    int gcdValue = gcd(C1, C2);
+    C1 /= gcdValue;
+    C2 /= gcdValue;
+    cout << C1 << " " << C2 << '\n';
 }

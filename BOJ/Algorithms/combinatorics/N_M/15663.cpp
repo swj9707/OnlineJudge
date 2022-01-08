0a01b892dd8f8f691d1a5e202aefcb5c39c4259a
swj1265 <swj1265@naver.com>
2022-01-08 13:43:32 +0900

    20220108

--- BOJ/Algorithms/combinatorics/N_M/15663.cpp
+++ BOJ/Algorithms/combinatorics/N_M/15663.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <set>
+#include <string>
 using namespace std;
 
 int main(){
     int N, M;
     cin >> N >> M;
     vector<int> vec(N);
+    set<string> st;
     for(int i = 0; i < N; i++) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    do{
+        string tmp = "";
+        for(int i = 0; i < M; i++){
+            tmp += to_string(vec[i]) + " ";
+        }
+        st.insert(tmp);
+    } while(next_permutation(vec.begin(), vec.end()));
+    for(auto i : st){
+        cout << i << '\n';
+    }
+    return 0;
 }

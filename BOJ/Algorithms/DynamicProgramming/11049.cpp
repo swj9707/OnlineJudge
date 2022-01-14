fc8911538293d55c97d906df25f1d3a4a02188be
swj1265 <swj1265@naver.com>
2022-01-14 14:34:13 +0900

    20220114

--- BOJ/Algorithms/DynamicProgramming/11049.cpp
+++ BOJ/Algorithms/DynamicProgramming/11049.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-
+#define MAX 501
 using namespace std;
 
+int DP[MAX][MAX];
+int MATRIX[MAX][2];
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N;
     cin >> N;
+    for(int i = 1; i <= N; i++){
+        cin >> MATRIX[i][0] >> MATRIX[i][1];
+    }
 }

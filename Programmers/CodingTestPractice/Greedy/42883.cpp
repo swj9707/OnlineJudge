2d0105c3d51c9b4dee0cb2d9c91aac1b9236d52e
swj1265 <swj1265@naver.com>
2022-01-17 16:44:16 +0900

    20220117

--- Programmers/CodingTestPractice/Greedy/42883.cpp
+++ Programmers/CodingTestPractice/Greedy/42883.cpp
@@ -8,7 +8,20 @@ using namespace std;
 string solution(string number, int k){
     string answer = "";
     int maxSize = number.length() - k;
-    
+    int start = 0;
+
+    for(int i = 0; i < maxSize; i++){
+        char maxNum = number[start];
+        int maxIndex = start;
+        for(int j = start; j <= k + i; j++){
+            if(maxNum < number[j]){
+                maxNum = number[j];
+                maxIndex = j;
+            }
+        }
+        start = maxIndex + 1;
+        answer += maxNum;
+    }
     return answer;
 }
 

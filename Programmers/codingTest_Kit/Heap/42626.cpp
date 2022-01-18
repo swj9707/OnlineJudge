d5f162e85dd2619875567ebec8262c51950c30a1
swj1265 <swj1265@naver.com>
2022-01-19 01:30:42 +0900

    20220119

---
+++ Programmers/codingTest_Kit/Heap/42626.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    for(int i = 0; i < scoville.size(); i++) PQ.push(scoville[i]); 
+    int count = 0;
+    while(PQ.size() > 1 && PQ.top() < K){
+        int first = PQ.top();
+        PQ.pop();
+        int second = PQ.top();
+        PQ.pop();
+        int result = first + (second * 2);
+        PQ.push(result);
+        count++;
+    }
+    if(PQ.top() < K) count = -1;
+    return count;
+}

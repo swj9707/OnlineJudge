13585cae4b191b56d1e4c114958aa9ef7106bfa1
swj1265 <swj1265@naver.com>
2022-01-19 02:42:30 +0900

    20220119

--- Programmers/codingTest_Kit/DFS_BFS/43162.cpp
+++ Programmers/codingTest_Kit/DFS_BFS/43162.cpp
@@ -3,8 +3,18 @@
 #define MAX 201
 using namespace std;
 
-int MATRIX[MAX][MAX];
 bool Visit[MAX];
+vector<int> vec[MAX];
+
+void DFS(int X){
+    for(int i = 0; i < vec[X].size(); i++){
+        int node = vec[X][i];
+        if(!Visit[node]){
+            Visit[node] = true;
+            DFS(node);
+        }
+    }
+}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
@@ -12,10 +22,16 @@ int solution(int n, vector<vector<int>> computers) {
         for(int j = 0; j < n; j++){
             if(i != j){
                 if(computers[i][j] == 1){
-                    MATRIX[i][j] = 1;
-                    MATRIX[j][i] = 1;
+                    vec[i].push_back(j);
                 }
             }
+        }
+    }
+
+    for(int i = 0; i < n ; i++){
+        if(!Visit[i]) {
+            DFS(i);
+            answer++;
         }
     }
     return answer;

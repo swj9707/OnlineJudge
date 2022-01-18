86e50492b7922f41bef1578150f647bb76f89450
swj1265 <swj1265@naver.com>
2022-01-19 02:22:27 +0900

    20220119

---
+++ Programmers/codingTest_Kit/Heap/42627.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> jobs) {
+    int answer = 0;
+    sort(jobs.begin(), jobs.end());
+    
+    return answer;
+}

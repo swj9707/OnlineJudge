80e618bdfb3166f1042ec83788d61e6575051ca7
swj1265 <swj1265@naver.com>
2022-01-18 19:01:47 +0900

    20220118

--- BOJ/Algorithms/Greedy/1343.cpp
+++ BOJ/Algorithms/Greedy/1343.cpp
@@ -2,7 +2,47 @@
 #include <string>
 using namespace std;
 
-int main(){
+int main()
+{
     string inputData;
-    getline(cin, inputData);
+    cin >> inputData;
+    string answer = "";
+    int i = 0;
+    int count = 0;
+    while (inputData[i] != '\0')
+    {
+        while (inputData[i] == 'X' || inputData[i] != '.')
+        {
+            i++;
+            count++;
+            if (inputData[i] == '\0')
+            {
+                break;
+            }
+        }
+        if (count % 2 == 0)
+        {
+            while (4 <= count)
+            {
+                answer += "AAAA";
+                count -= 4;
+            }
+            while (2 <= count)
+            {
+                answer += "BB";
+                count -= 2;
+            }
+        }
+        if (count != 0)
+        {
+            cout << -1 << '\n';
+            return 0;
+        }
+        if (inputData[i] == '.')
+        {
+            answer += '.';
+            i++;
+        }
+    }
+    cout << answer << '\n';
 }

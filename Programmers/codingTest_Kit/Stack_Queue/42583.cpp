459d08a976711d5ce174d1bedc5d76bbebd5e3f7
swj1265 <swj1265@naver.com>
2022-01-20 09:07:57 +0900

    20220120

--- Programmers/codingTest_Kit/Stack_Queue/42583.cpp
+++ Programmers/codingTest_Kit/Stack_Queue/42583.cpp
@@ -1,33 +1,33 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
+
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    vector<int> after;
-    queue<int> bridge;
-    int sum = 0, idx = 0;
-    int trucks = truck_weights.size();
-    for(int i = 0; i < bridge_length ; i++) bridge.push(-1);
-    while(after.size() != trucks){
-        if(sum + truck_weights[idx] < weight){
-            sum += truck_weights[idx];
-            bridge.pop();
-            bridge.push(truck_weights[idx]);
-            idx++;
-        }
-        else{
-            if(bridge.front() != -1){ 
-                sum -= bridge.front();
-                after.push_back(bridge.front());
-            }
-            bridge.pop();
-            bridge.push(-1);
+    
+    int idx = 0, sum = 0;
+    queue<int> q;
+
+    while(1){
+        if(idx == truck_weights.size()){
+            answer += truck_weights.size();
+            break;
         }
         answer++;
-        cout << idx << '\n';
+        int tmp = truck_weights[idx];
+        if(q.size() == bridge_length){
+            sum -= q.front();
+            q.pop();
+        }
+        if(sum + tmp <= weight){
+            q.push(tmp);
+            sum += tmp;
+            idx++;
+        }else{
+            q.push(0);
+        }
     }
 
     return answer;

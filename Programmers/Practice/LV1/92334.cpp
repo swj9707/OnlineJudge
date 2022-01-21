f0e51d872c672bb425d0e0e8e3827e52491577d3
swj1265 <swj1265@naver.com>
2022-01-22 00:18:57 +0900

    20220122

--- Programmers/Practice/LV1/92334.cpp
+++ Programmers/Practice/LV1/92334.cpp
@@ -2,11 +2,46 @@
 using namespace std;
 
 vector<vector<bool>> reportList;
-map<string, int> user;
-vector<int> numberOfReport;
+map<string, int> userList;
+vector<int> reportCount;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
-    vector<int> answer;
+    vector<int> answer, list;
+    int length = id_list.size();
+    list.reserve(length);
+    reportList.resize(length, vector<bool>(length, false));
+    reportCount.resize(length, 0);
+    answer.resize(length, 0);
+
+    for(int i = 0; i < length; i++){
+        userList[id_list[i]] = i;   
+    }
+
+    for(auto i : report){
+        istringstream ss(i);
+        string tmp[2];
+        ss >> tmp[0] >> tmp[1];
+
+        string reporter = tmp[0];
+        string respondent = tmp[1];
+        reportList[userList[reporter]][userList[respondent]] = true;
+    }
+
+    for(int i = 0; i < length; i++){
+        for(int j = 0; j < length ; j++){
+            if(reportList[i][j]) reportCount[j]++;
+        }
+    }
+
+    for(int i = 0; i < length ; i++){
+        if(reportCount[i] >= k) list.push_back(i);
+    }
+
+    for(int i = 0; i < length; i++){
+        for(auto idx : list){
+            if(reportList[i][idx]) answer[i] += 1;
+        }
+    }
 
     return answer;
 }
--- Programmers/codingTest_Kit/Heap/42627.cpp
+++ Programmers/codingTest_Kit/Heap/42627.cpp
@@ -7,5 +7,24 @@ using namespace std;
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
     sort(jobs.begin(), jobs.end());
-    return answer;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    int idx = 0, time = 0;
+    int length = jobs.size();
+    while(idx < length || !PQ.empty()){
+        if(idx < length && time >= jobs[idx][0]){
+            PQ.push({jobs[idx][1], jobs[idx][0]});
+            idx++;
+            continue;
+        }
+        if(!PQ.empty()){
+            time += PQ.top().first;
+            answer += time - PQ.top().second;
+            PQ.pop();
+        }
+        else{
+            time = jobs[idx][0];
+        }
+    }
+
+    return answer / length;
 }

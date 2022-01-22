b54cdd33a510ba224e13d54689f612923888113d
swj1265 <swj1265@naver.com>
2022-01-22 17:00:12 +0900

    20220122

--- BOJ/Algorithms/Greedy/11508.cpp
+++ BOJ/Algorithms/Greedy/11508.cpp
@@ -3,12 +3,21 @@
 #include <algorithm>
 using namespace std;
 
+bool cmp(int a, int b){
+    return a > b;
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N;
-    vector<int> vec(N);
     cin >> N;
+    vector<int> vec(N);
     for(int i = 0; i < N; i++) cin >> vec[i];
-    sort(vec.begin(), vec.end());
+    sort(vec.begin(), vec.end(), cmp);
+    long long answer = 0;
+    for(int i = 0; i < N ; i++){
+        if (i % 3 != 2) answer += vec[i];
+    }
+    cout << answer << '\n';
 }

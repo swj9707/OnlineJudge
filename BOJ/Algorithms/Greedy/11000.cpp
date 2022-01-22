221ce435ce8285ce2f4e7662acdd7b187f760c70
swj1265 <swj1265@naver.com>
2022-01-22 15:21:51 +0900

    20220122

---
+++ BOJ/Algorithms/Greedy/11000.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    vector<pair<int, int>> classes(N);
+    for(int i = 0; i < N; i++) cin >> classes[i].first >> classes[i].second;
+    sort(classes.begin(), classes.end());
+
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    PQ.push(classes[0].second);
+    for(int i = 1; i < N; ++i){
+        if(PQ.top() <= classes[i].first){
+            PQ.pop();
+            PQ.push(classes[i].second);
+        }
+        else{
+            PQ.push(classes[i].second);
+        }
+    }
+
+    cout << PQ.size() << '\n';
+}

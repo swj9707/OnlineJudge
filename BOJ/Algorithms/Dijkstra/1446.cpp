9b2e7ab42fdb48822af87ff8bd85ede128be240f
SierraSon97 <swj1265@naver.com>
2022-01-23 02:07:00 +0900

    20220123

--- BOJ/Algorithms/Dijkstra/1446.cpp
+++ BOJ/Algorithms/Dijkstra/1446.cpp
@@ -9,28 +9,6 @@ int Dist[MAX];
 int N, D;
 vector<pair<int, int>> edge[MAX];
 
-void Dijkstra(int Start){
-    for (int i = 1; i <= N; i++) Dist[i] = INF;
-    priority_queue<pair<int, int>> PQ;
-    PQ.push({0, Start});
-    Dist[Start] = 0;
-    while (!PQ.empty()){
-        int Cost = PQ.top().first;
-        int Cur = PQ.top().second;
-        PQ.pop();
-        if (Dist[Cur] < Cost) continue;
-        for (int i = 0; i < edge[Cur].size(); i++){
-            int Next = edge[Cur][i].first;
-            int nCost = edge[Cur][i].second;
-            if (Dist[Next] > Cost + nCost){
-                Dist[Next] = Cost + nCost;
-                PQ.push({Dist[Next], Next});
-                cout << Cur << " " << Next << '\n';
-            }
-        }
-    }
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -40,5 +18,21 @@ int main(){
         cin >> src >> dst >> cost;
         edge[src].push_back({dst, cost});
     }
-    //Dijkstra(0);
+    int before;
+    for(int i = 0; i <= D; i++){
+        if(i == 0) before = -1;
+        else before = Dist[i-1];
+        Dist[i] = min(Dist[i], before+1);
+        if(!edge[i].empty()){
+            for(int j = 0; j < edge[i].size(); j++){
+                int cur = edge[i][j].first;
+                int cost = edge[i][j].second;
+                if(cur > D) continue;
+                if(Dist[i] + cost < Dist[cur]){
+                    Dist[cur] = Dist[i] + cost;
+                }
+            }
+        }
+    }
+    cout << Dist[D];
 }

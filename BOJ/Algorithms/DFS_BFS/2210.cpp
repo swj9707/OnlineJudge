415a5a57fd866904fd297d89427aca93930156ed
SierraSon97 <swj1265@naver.com>
2022-01-24 23:33:53 +0900

    20220124

--- BOJ/Algorithms/DFS_BFS/2210.cpp
+++ BOJ/Algorithms/DFS_BFS/2210.cpp
@@ -5,25 +5,32 @@
 
 using namespace std;
 
-int MATRIX[5][5];
+int MATRIX[6][6];
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 set <string> result;
 
-void BFS(int Y, int X){
-    deque<pair<int, int>> DQ;
-    DQ.push_back({Y, X});
-    int count = 5;
-    while(count > 5){
-        
+void BFS(int X, int Y){
+    deque<pair<string, pair<int, int>>> DQ;
+    DQ.push_back({to_string(MATRIX[X][Y]), {X, Y}});
+    while(!DQ.empty()){
+        string data = DQ.front().first;
+        int x = DQ.front().second.first;
+        int y = DQ.front().second.second;
+        if(data.length() == 6)
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+        }
     }
+    
 }
 
 int main(){
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++) cin >> MATRIX[i][j];
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= 5; j++) cin >> MATRIX[i][j];
     }
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++) cin >> MATRIX[i][j];
-    }
+
 }

a6751ffd6399de6009e6bda744ca425925ff60a5
swj1265 <swj1265@naver.com>
2022-01-26 22:13:08 +0900

    20220126

--- BOJ/Algorithms/Greedy/19939.cpp
+++ BOJ/Algorithms/Greedy/19939.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N, K;
+    cin >> N >> K;
+    int sum = 0;
+    for(int i = 1; i <= K; i++) sum += i;
+    if(sum > N) cout << "-1\n";
+    else{
+        if(N % K == 0) cout << K - 1 << '\n';
+        else cout << K << '\n';
+    }
 }

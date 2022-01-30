6c99ab4d781a22a8bcf3940d1c618bb69aa7111f
swj1265 <swj1265@naver.com>
2022-01-30 22:42:27 +0900

    20220130

--- Programmers/codingTest_Kit/Hash/42578.cpp
+++ Programmers/codingTest_Kit/Hash/42578.cpp
@@ -1,18 +1,13 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
-    map<string, int> m;
-    for(vector<string> c : clothes){
-        m[c[1]]++;
-    }
     int answer = 1;
-    map<string, int>::iterator it;
-    for(it = m.begin(); it != m.end(); it++){
-        answer *= it->second + 1;
-    }
+    unordered_map<string, int> uom;
+    for(auto i : clothes) uom[i[1]]++;
+    for(auto i : uom) answer *= (i.second + 1);
     return answer-1;
 }

ca1b4a6a4fc408b1c0afb0323ec71cb7efba5f67
swj1265 <swj1265@naver.com>
2022-01-31 10:50:55 +0900

    20220131

--- Programmers/codingTest_Kit/Hash/42579.cpp
+++ Programmers/codingTest_Kit/Hash/42579.cpp
@@ -1,9 +1,23 @@
 #include <string>
 #include <vector>
-
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
+bool cmp(pair<int, int> a, pair<int, int> b){
+    return a.first > b.first;
+}
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
+    unordered_map<string, vector<pair<int, int>>> uom;
+    int N = genres.size();
+    for(int i = 0 ; i < N; i++){
+        uom[genres[i]].push_back({plays[i], i});
+    }
+    
     return answer;
+}
+
+int main(){
+    solution({"classic", "pop", "classic", "classic", "pop"},{500, 600, 150, 800, 2500});
 }

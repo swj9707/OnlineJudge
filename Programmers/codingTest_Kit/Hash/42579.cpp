392d728ef9deff39ab0ddc6138a00ca500097b9d
swj1265 <swj1265@naver.com>
2022-01-31 11:08:25 +0900

    20220131

--- Programmers/codingTest_Kit/Hash/42579.cpp
+++ Programmers/codingTest_Kit/Hash/42579.cpp
@@ -9,12 +9,38 @@ bool cmp(pair<int, int> a, pair<int, int> b){
 }
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    unordered_map<string, vector<pair<int, int>>> uom;
+    unordered_map<string, unordered_map<int, int>> musicList;
+    unordered_map<string, int> music;
     int N = genres.size();
-    for(int i = 0 ; i < N; i++){
-        uom[genres[i]].push_back({plays[i], i});
+    for(int i = 0 ; i < N; i++) {
+        musicList[genres[i]][i] = plays[i];
+        music[genres[i]] += plays[i];
     }
-    
+    while(music.size()){
+        string genre;
+        int max = 0;
+        for(auto mu : music){
+            if(max < mu.second){
+                max = mu.second;
+                genre = mu.first;
+            }
+        }
+        for(int i = 0; i < 2; i++){
+            int max = 0, index = -1;
+            for(auto data : musicList[genre]){
+                if(max < data.second){
+                    max = data.second;
+                    index = data.first;
+                }
+            }
+            if(index != -1){
+                answer.push_back(index);
+                musicList[genre].erase(index);
+            }
+            else break;
+        }
+        music.erase(genre);
+    }
     return answer;
 }
 

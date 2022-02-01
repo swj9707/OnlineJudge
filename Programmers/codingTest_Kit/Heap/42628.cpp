25c7fe70e075f64acf9d863edab341a1f9a0784f
SierraSon97 <swj1265@naver.com>
2022-02-01 18:27:40 +0900

    20220201

---
+++ Programmers/codingTest_Kit/Heap/42628.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    vector<int> tmp;
+    priority_queue<int, vector<int>, greater<int>> PQ;
+    int N = operations.size();
+    bool isGreater = true;
+    for(int i = 0; i < N; i++){
+        string oper = operations[i];
+
+    }
+    return answer;
+}

67b65aa80498df537459e79e2d2f14dcdd9e48b6
SierraSon97 <swj1265@naver.com>
2022-02-02 19:53:54 +0900

    20220202

--- Programmers/codingTest_Kit/Greedy/42860.cpp
+++ Programmers/codingTest_Kit/Greedy/42860.cpp
@@ -5,33 +5,14 @@ using namespace std;
 
 int solution(string name) {
     int answer = 0;
+    for(auto i : name) answer += min(i - 'A', 'Z' + 1 - i);
     int len = name.length();
-    int idx = 0;
-    string tmp(len, 'A');
-    while(name != tmp){
-        tmp[idx] = name[idx];
-        answer += min(name[idx] - 'A', 'Z' + 1 - name[idx]);
-        if(name == tmp) break;
-        int left = idx, right = idx;
-        int left_count = 0, right_count = 0;
-        while(tmp[left] == name[left]){
-            left--;
-            left_count++;
-            if(left < 0) left = len - 1;
-        }
-        while(tmp[right] == name[right]){
-            right;
-            right_count++;
-            if(right >= len) right = 0;
-        }
-        if(left_count > right_count) {
-            idx = right;
-            answer += right_count;
-        }
-        else {
-            idx = left;
-            answer += left_count;
-        }
+    int move = len - 1;
+    for(int i = 0; i < len; i++){
+        int next = i + 1;
+        while(next < len && name[next] == 'A') next++;
+        move = min(move, i + (len - next) + min(i, len-next));
     }
+    answer += move;
     return answer;
 }

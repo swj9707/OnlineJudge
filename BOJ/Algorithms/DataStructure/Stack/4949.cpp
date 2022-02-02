2fc4bfd277317d23601bf63893784b1ac7434784
SierraSon97 <swj1265@naver.com>
2022-02-02 20:28:42 +0900

    20220202

--- BOJ/Algorithms/DataStructure/Stack/4949.cpp
+++ BOJ/Algorithms/DataStructure/Stack/4949.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <stack>
+
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    string tmp = "";
-    stack<char> stk;
+    string tmp;
     while(true){
         getline(cin, tmp);
         if(tmp == ".") break;
-        while(!stk.empty()) stk.pop();
+        stack<char> stk;
+        bool check = true;
         for(auto i : tmp){
             if(i == '[' || i == '(') stk.push(i);
-            else{
-                if((i == ']' && stk.top() == '[') || 
-                (i == ')' && stk.top() == '(')) stk.pop();
+            else if(i == ')'){
+                if(!stk.empty() && stk.top() == '(') stk.pop();
+                else{
+                    check = false;
+                    break;
+                }
+            }
+            else if(i == ']'){
+                if(!stk.empty() && stk.top() == '[') stk.pop();
+                else{
+                    check = false;
+                    break;
+                }
             }
         }
-        if(stk.empty()) cout << "yes\n";
+        if(check && stk.empty()) cout << "yes\n";
         else cout << "no\n";
     }
 }

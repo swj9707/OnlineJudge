cefb53f3188a6c4c912d8d135038fe01225b9d9b
SierraSon97 <swj1265@naver.com>
2022-02-05 13:15:03 +0900

    20220205

---
+++ Programmers/codingTest_Kit/Stack_Queue/42584.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    int N = prices.size();
+    vector<int> answer(N);
+    stack<int> stk;
+    for (int i = 0; i < N; i++) {
+        while (!stk.empty() && prices[stk.top()] > prices[i]) {
+            answer[stk.top()] = i - stk.top();
+            stk.pop();
+        }
+        stk.push(i);
+    }
+    while (!stk.empty()) {
+        answer[stk.top()] = N - stk.top() - 1;
+        stk.pop();
+    }
+    return answer;
+}

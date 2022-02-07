d2ed056adb0dd6405619e8fa669b7a287b0f2f39
SierraSon97 <swj1265@naver.com>
2022-02-07 11:24:31 +0900

    20220207

--- BOJ/Algorithms/Greedy/13413.cpp
+++ BOJ/Algorithms/Greedy/13413.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
@@ -8,6 +8,19 @@ int main(){
     int T;
     cin >> T;
     while(T--){
-        
+        int N;
+        string src;
+        string dst;
+        int WCount = 0;
+        int BCount = 0;
+        cin >> N >> src >> dst;
+        for(int i = 0; i < N; i++){
+            if(src.at(i) != dst.at(i)){
+                if(src.at(i) == 'B') BCount++;
+                else WCount++;
+            }
+        }
+        if(BCount < WCount) cout << WCount << '\n';
+        else cout << BCount << '\n';
     }  
 }

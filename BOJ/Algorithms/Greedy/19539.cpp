2121270085cf3870c4153f592ab4acdf056d6791
SierraSon97 <swj1265@naver.com>
2022-02-07 18:42:44 +0900

    20220207

--- BOJ/Algorithms/Greedy/19539.cpp
+++ BOJ/Algorithms/Greedy/19539.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+int arr[100001];
+
 int main(){
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> arr[i];
+    int one = 0, two = 0;
+
+    for(int i = 1; i <= N; i++){
+        two += arr[i] / 2;
+        one += arr[i] % 2;
+    }
+    if((two - one) % 3 == 0 && two >= one){
+        cout << "YES\n";
+    }
+    else cout << "NO\n";
 }

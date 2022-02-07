57db3c9651e18847a0e6781e272c91c80f3bd910
SierraSon97 <swj1265@naver.com>
2022-02-07 15:38:37 +0900

    20220207

--- BOJ/Algorithms/Greedy/12018.cpp
+++ BOJ/Algorithms/Greedy/12018.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -7,5 +8,29 @@ int main(){
     cin.tie(0);
     int N, M;
     cin >> N >> M;
-    
+    vector<int> result;
+    int answer = 0;
+    while(N--){
+        int P, L;
+        cin >> P >> L;
+        vector<int> vecP(P);
+        for(int i = 0; i < P; i++) cin >> vecP[i];
+        sort(vecP.begin(), vecP.end(), greater<int>());
+        if(P >= L){
+            int lowerbound = vecP[L - 1];
+            result.push_back(lowerbound);
+        }
+        else{
+            result.push_back(1);
+        }
+    }
+    sort(result.begin(), result.end());
+    for(auto i : result){
+        if(M - i >= 0){
+            M -= i;
+            answer++;
+        }
+        else break;
+    }
+    cout << answer << '\n';
 }

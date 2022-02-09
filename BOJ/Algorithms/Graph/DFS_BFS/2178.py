18703c910037ca84248ed766315fb3a4b21e11dc
SierraSon97 <swj1265@naver.com>
2022-02-09 11:06:47 +0900

    20220209

--- BOJ/Algorithms/Graph/DFS_BFS/2178.py
+++ BOJ/Algorithms/Graph/DFS_BFS/2178.py
@@ -1,25 +1,24 @@
 import sys ; input = lambda : sys.stdin.readline().rstrip()
 
-N, M = map(int, input().split())
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-board = [list(input()) for _ in range(N)]
-board[0][0] = 1
-queue = [[0,0]]
-def bfs(queue, board):
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+def BFS(board, N, M):
+    board[0][0] = 1
+    queue = [[0,0]]
     while queue:
-        x, y = queue[0][0], queue[0][1]
+        X, Y = queue[0][0], queue[0][1]
         del queue[0]
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx >= 0 and ny >= 0 and nx < N and ny < M:
+            nx = X + dx[i]
+            ny = Y + dy[i]
+            if(nx >= 0 and ny >= 0 and nx < N and ny < M):
                 if board[nx][ny] == '1':
-                    queue.append([nx,ny])
-                    board[nx][ny] = board[x][y] + 1
+                    queue.append([nx, ny])
+                    board[nx][ny] = board[X][Y] + 1
 
-bfs(queue, board)
+N, M = map(int, input().split())
 
+board = [list(input()) for _ in range(N)]
+
+BFS(board, N, M)
 print(board[N-1][M-1])

6ab918aac812fd3ddf154ec53d4541309f9db918
SierraSon97 <swj1265@naver.com>
2022-02-10 20:43:47 +0900

    20220210

--- BOJ/Algorithms/Greedy/20937.cpp
+++ BOJ/Algorithms/Greedy/20937.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #define MAX 500001
 using namespace std;
-int count[MAX];
+int Count[MAX];
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N;
     cin >> N;
-    int answer = 0;
+    int answer = -1;
     vector<int> data(N);
     for(int i = 0; i < N; i++) cin >> data[i];
-    for(int i = 0; i < N; i++) count[data[i]] += 1;
-    for(int i = 0; i < N; i++) answer = max(answer, count[i]);
+    for(int i = 0; i < N; i++) Count[data[i]] += 1;
+    for(int i = 0; i < MAX; i++) answer = max(answer, Count[i]);
     cout << answer << '\n';
 }

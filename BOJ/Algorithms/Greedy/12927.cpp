cdedb4cb3538783e989eb75bf84b43c37004c324
SierraSon97 <swj1265@naver.com>
2022-02-10 23:13:21 +0900

    20220210

---
+++ BOJ/Algorithms/Greedy/12927.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string bulb = "N";
+    string tmp;
+    cin >> tmp;
+    bulb += tmp;
+    int count = 0;
+    for(int i = 1; i < bulb.length(); i++){
+        if(bulb[i] == 'Y'){
+            for (int j = i; j < bulb.length(); j += i){
+                if(bulb[j] == 'Y') bulb[j] = 'N';
+                else bulb[j] = 'Y';
+            }
+            count += 1;
+        }
+    }
+    cout << count << '\n';
+}

e5ff777ea4f41eb94158c27a555d38b1de5524a6
SierraSon97 <swj1265@naver.com>
2022-02-11 17:48:43 +0900

    20220211

--- BOJ/Algorithms/Graph/1453.py
+++ BOJ/Algorithms/Graph/1453.py
@@ -2,10 +2,10 @@ import sys; input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 dataList = list(map(int, input().split()))
-seat = [0] * (N+1)
+seat = [0] * 101
 for i in dataList:
     if seat[i] == 0:
         seat[i] = 1
 
-answer = sum(seat) - N
+answer = N - sum(seat)
 print(answer)

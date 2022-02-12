48bc60dda8e14203819efbc7bc57a99362671a0e
SierraSon97 <swj1265@naver.com>
2022-02-12 13:38:13 +0900

    20220212

--- BOJ/Algorithms/Greedy/2777.py
+++ BOJ/Algorithms/Greedy/2777.py
@@ -1,7 +1,33 @@
 import sys ; input = lambda : sys.stdin.readline().rstrip()
 
+def factorize(n):
+    factor = 2 
+    factors = []
+    while (factor**2 <= n): 
+        while (n % factor == 0):
+            if factor < 11:
+                factors.append(factor) 
+                n = n // factor  
+            else:
+                factors.clear()
+                return factors
+        factor += 1
+    if n > 1 :
+        if n < 11:
+            factors.append(n)
+        else:
+            factors.clear()
+    return factors
+
 T = int(input())
+
+
 
 for _ in range(T):
     N = int(input())
+    dataList = factorize(N)
+    if len(dataList) == 0:
+        print(-1)
+    else:
+        print(dataList)
     

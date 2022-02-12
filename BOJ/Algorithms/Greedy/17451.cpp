c861cc3552170712a6783a4aa798ec731465bcef
SierraSon97 <swj1265@naver.com>
2022-02-12 14:19:37 +0900

    20220212

--- BOJ/Algorithms/Greedy/17451.cpp
+++ BOJ/Algorithms/Greedy/17451.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long answer = 0;
+    int N;
+    cin >> N;
+    vector<long long> vec(N);
+    for(int i = 0; i < N; i++) cin >> vec[i];
+    for(int i = N - 1; i >= 0; i--){
+        if(answer <= vec[i]) answer = vec[i];
+        else{
+            if(answer % vec[i]){
+                answer = (answer / vec[i] + 1) * vec[i];
+            }
+        }
+        cout << answer << " " << vec[i] << '\n';
+    }
+    cout << answer << '\n';
 }
--- BOJ/Algorithms/Greedy/17451.py
+++ BOJ/Algorithms/Greedy/17451.py
@@ -1,4 +1,13 @@
 import sys ; input = lambda : sys.stdin.readline().rstrip()
 
-N = int(input())
-dataList = list(map(int, input().split()))
+n = int(input())
+v = list(map(int, input().split()))
+answer = 0
+for i in range(n-1, -1, -1):
+    if answer <= v[i]:
+        answer = v[i]
+    else:
+        if answer % v[i]:
+            answer = (answer // v[i] + 1) * v[i]
+    print(answer, v[i])
+print(answer)

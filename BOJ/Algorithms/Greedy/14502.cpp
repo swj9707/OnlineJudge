2725a6ee72ee953f482b89ede910fb2d28b6bf6b
SierraSon97 <swj1265@naver.com>
2022-02-12 18:08:20 +0900

    20220212

--- BOJ/Algorithms/Greedy/14502.cpp
+++ BOJ/Algorithms/Greedy/14502.cpp
@@ -1,82 +1,70 @@
 #include <iostream>
 #include <queue>
-
+#include <vector>
 using namespace std;
 
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+int N, M;
+int MATRIX[MAX][MAX];
 int answer = 0;
-void check_bfs(vector<vector<int>>& v) {
-	queue<pair<int, int>> q;
-
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = 0; j < v[i].size(); j++) {
-			if (v[i][j] == 2)
-				q.emplace(i, j);
-		}
-	}
-
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int next_x = x + dx[i];
-			int next_y = y + dy[i];
-
-			if (next_x > -1 && next_x < v.size() && next_y > -1 && next_y < v[x].size() &&
-				v[next_x][next_y] == 0) {
-				q.emplace(next_x, next_y);
-				v[next_x][next_y] = 2;
-			}
-		}
-	}
-
-	int cnt = 0;
-	for (int i = 0; i < v.size(); i++) {
-		for (int j = 0; j < v[i].size(); j++) {
-			if (v[i][j] == 0)
-				cnt++;
-		}
-	}
-	answer = max(answer, cnt);
+void BFS(vector<vector<int>> & vec){
+    queue<pair<int, int>> Q;
+    int count = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(vec[i][j] == 2) Q.push({i, j});
+        }
+    }
+    while(!Q.empty()){
+        int X = Q.front().first;
+        int Y = Q.front().second;
+        Q.pop();
+        for(int i = 0; i < 4; i++){
+            int nx = X + dx[i];
+            int ny = Y + dy[i];
+            if(nx >= 0 && nx < N && ny >= 0 && ny < M && vec[nx][ny] == 0){
+                vec[nx][ny] = 2;
+                Q.push({nx, ny});
+            }
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(vec[i][j] == 0) count++;
+        }
+    }
+    answer = max(answer, count);
 }
 
-void dfs(vector<vector<int>> v, int x, int y, int cnt){
-	v[x][y] = 1;
-
-	if (cnt == 0) {
-		check_bfs(v);
-		return;
-	}
-
-	for (int i = x; i < v.size(); i++) {
-		for (int j = 0; j < v[x].size(); j++) {
-			if (v[i][j] == 0)
-				dfs(v, i, j, cnt - 1);
-		}
-	}
+void DFS(vector<vector<int>> vec, int x, int y, int count){
+    vec[x][y] = 1;
+    if(count == 0){
+        BFS(vec);
+        return;
+    }
+    for(int i = x ; i < N ; i++){
+        for(int j = 0; j < M; j++){
+            if(vec[i][j] == 0) DFS(vec, i, j, count -1);
+        }
+    }
 }
 
-int main() {
-	int N, a, b, M;
-	
-	cin >> N >> M;
-	
-	vector<vector<int>> v(N, vector<int>(M));
-
-	for (int i = 0; i < N; i++) {
-		for(int j = 0; j < M; j++)
-			scanf("%d", &v[i][j]);
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (v[i][j] == 0)
-				dfs(v, i, j, 2);
-		}
-	}
-	
-	cout << answer << endl;
-	return 0;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    vector<vector<int>> vec(N, vector<int>(M));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cin >> vec[i][j];
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(vec[i][j] == 0) DFS(vec, i, j, 2);
+        }
+    }
+    cout << answer << '\n';
+    return 0;
 }

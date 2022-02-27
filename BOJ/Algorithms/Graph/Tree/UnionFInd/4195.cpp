aa6960ccdbb4c15465138fe2bb436bb3aa6ef8b1
SierraSon97 <swj1265@naver.com>
2022-02-27 18:15:10 +0900

    20220227

---
+++ BOJ/Algorithms/Graph/Tree/UnionFInd/4195.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T; cin >> T;
+    while(T--){
+        int N; cin >> N;
+        
+    }
+}

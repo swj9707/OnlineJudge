ab55b815855835afc73d8b31970a6a8b61abb7c5
SierraSon97 <swj1265@naver.com>
2022-03-01 14:52:46 +0900

    20220301

--- BOJ/Algorithms/TwoPointer/17609.cpp
+++ BOJ/Algorithms/TwoPointer/17609.cpp
@@ -2,20 +2,29 @@
 
 using namespace std;
 
+int isPalindrome(int left, int right, int del, string tmp){
+    while(left < right){
+        if(tmp[left] != tmp[right]){
+            if(del == 1){
+                if(isPalindrome(left+1, right, 0, tmp) == 0 || isPalindrome(left, right-1, 0, tmp) == 0) return 1;
+                return 2;
+            }
+            else return 2;
+        }
+        else{
+            left++; right--;
+        }
+    }
+    return 0;
+}
+
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N; cin >> N;
     while(N--){
         string tmp; cin >> tmp;
-        int left = 0, right = tmp.length() - 1;
-        int answer = 0, count = 0;
-        while(left < right){
-            if(tmp[left] == tmp[right]) count++;
-            left++; right--;
-        }
-        int halfLength = tmp.length() / 2;
-        if(halfLength == count) answer = 0;
-        else if(halfLength == count + 1) answer = 1;
+        int answer = isPalindrome(0, tmp.length()-1, 1, tmp);
+        cout << answer << '\n';
     }
 }

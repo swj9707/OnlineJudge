dd3aa364f74fb7a63cfde7bf86c64902b664869b
SierraSon97 <swj1265@naver.com>
2022-03-03 16:06:58 +0900

    20220303

---
+++ BOJ/Algorithms/Graph/Dijkstra/10282.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#define MAX 10001
+#define INF 9876543
+using namespace std;
+
+int Dist[MAX];
+vector<pair<int, int>> edge[MAX];
+void initSet(){
+    for(int i = 0; i < MAX; i++) Dist[i] = INF;
+    for(int i = 0; i < MAX; i++) edge[i].clear();
+}
+
+void Dijkstra(int N, int C){
+    Dist[C] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> PQ;
+    PQ.push({0, C});
+    while(!PQ.empty()){
+        int Current = PQ.top().second;
+        int Cost = PQ.top().first;
+        PQ.pop();
+        if(Dist[Current] < Cost) continue;
+        for(int i = 0; i < edge[Current].size(); i++){
+            int Next = edge[Current][i].first;
+            int nCost = edge[Current][i].second + Cost;
+            if(Dist[Next] > nCost){
+                Dist[Next] = nCost;
+                PQ.push({nCost, Next});
+            }
+        }
+    }
+    int count = 0, answer = 0;
+    for(int i = 1; i <= N; i++){
+        if(Dist[i] != INF){
+            count++;
+            answer = max(answer, Dist[i]);
+        }
+    }
+    cout << count << " " << answer << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int TestCase; cin >> TestCase;
+    while(TestCase--){
+        initSet();
+        int N, D, C;
+        cin >> N >> D >> C;
+        for(int i = 0; i < D; i++){
+            int src, dst, cost;
+            cin >> src >> dst >> cost;
+            edge[dst].push_back({src, cost});
+        }
+        Dijkstra(N, C);
+    }
+}

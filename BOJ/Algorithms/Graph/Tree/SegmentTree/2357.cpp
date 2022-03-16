fc338cceb2e6e79cef76507512c9447d1ead0e5a
SierraSon97 <swj1265@naver.com>
2022-03-16 11:42:00 +0900

    20220316

--- BOJ/Algorithms/Graph/Tree/SegmentTree/2357.cpp
+++ BOJ/Algorithms/Graph/Tree/SegmentTree/2357.cpp
@@ -1,52 +1,69 @@
 #include <iostream>
+#include <cmath>
 #define MAX 100001
 #define MAXVALUE 1000000001
-#define ll long long
 using namespace std;
+long long maxTree[4 * MAX];
+long long minTree[4 * MAX];
+long long arr[MAX];
 
-ll arr[MAX];
-ll maxTree[MAX * 4];
-ll minTree[MAX * 4];
-
-ll maxinit(int start, int end, int node){
-    if(start == end) return maxTree[node] = arr[start];
-    int mid = (start + end) / 2;
-    return maxTree[node] = max(maxinit(start, mid, node * 2), maxinit(mid + 1, end, node * 2 + 1));
-}
-
-ll mininit(int start, int end, int node){
-    if(start == end) return minTree[node] = arr[start];
-    int mid = (start + end) / 2;
-    return minTree[node] = min(mininit(start, mid, node * 2), mininit(mid + 1, end, node * 2 + 1));
-}
-
-ll findValue(int start, int end, int left, int right, int node, bool maximum){
-    if(left > end || right < start){
-        if(maximum) return MAXVALUE;
-        else return -MAXVALUE;
-    }
-    if(left <= start && end <= right){
-        if(maximum) return maxTree[node];
-        else return minTree[node];
+int init(int start, int end, int node, bool maximum)
+{
+    if (start == end)
+    {
+        if (maximum)
+            return maxTree[node] = arr[start];
+        else
+            return minTree[node] = arr[start];
     }
     int mid = start + (end - start) / 2;
-    if(maximum) return max(findValue(start, mid, left, right, node * 2, maximum),findValue(mid+1, end, left, right, node * 2 + 1, maximum));
-    else return min(findValue(start, mid, left, right, node * 2, maximum),findValue(mid+1, end, left, right, node * 2 + 1, maximum));
+    if (maximum)
+        return maxTree[node] = max(init(start, mid, 2 * node, maximum), init(mid + 1, end, 2 * node + 1, maximum));
+    return minTree[node] = min(init(start, mid, 2 * node, maximum), init(mid + 1, end, 2 * node + 1, maximum));
 }
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N, M; cin >> N >> M;
-    for(int i = 0; i < MAX * 4 ; i++) minTree[i] = MAXVALUE;
-    maxinit(0, N-1, 1);
-    mininit(0, N-1, 1);
-    for(int i = 0; i < N; i++) cin >> arr[i];
-    for(int i = 0; i < M; i++){
-        int a, b; cin >> a >> b;
-        int left, right;
-        right = a > b ? a : b;
-        left = a > b ? b : a;
-        cout << findValue(0, N-1, left - 1, right - 1, 1, false) << " " << findValue(0, N-1, left-1, right-1, 1, true) << '\n';
+int findVal(int start, int end, int left, int right, int node, bool maximum)
+{
+    if (left > end || right < start)
+    {
+        if (maximum)
+            return -MAXVALUE;
+        return MAXVALUE;
     }
+    if (left <= start && end <= right)
+    {
+        if (maximum)
+            return maxTree[node];
+        else
+            return minTree[node];
+    }
+    int mid = (start + end)/2;
+    if (maximum)
+        return max(findVal(start, mid, left, right, node * 2, maximum), findVal(mid + 1, end, left, right, node * 2 + 1, maximum));
+    return min(findVal(start, mid, left, right, node * 2, maximum), findVal(mid + 1, end, left, right, node * 2 + 1, maximum));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < N * 4; i++)
+        minTree[i] = MAX;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    init(0, N - 1, 1, true);
+    init(0, N - 1, 1, false);
+
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        int right = max(a, b);
+        int left = min(a, b);
+        cout << findVal(0, N - 1, left - 1, right - 1, 1, false) << " " << findVal(0, N - 1, left - 1, right - 1, 1, true) << '\n';
+    }
+    return 0;
 }

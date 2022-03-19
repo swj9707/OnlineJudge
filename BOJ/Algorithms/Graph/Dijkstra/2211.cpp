1f7b01c7b20a65e956901dfcd0de21df3dabfcfd
SierraSon97 <swj1265@naver.com>
2022-03-19 10:08:55 +0900

    20220319

--- BOJ/Algorithms/Graph/Dijkstra/2211.cpp
+++ BOJ/Algorithms/Graph/Dijkstra/2211.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #define MAX 1001
 #define INF 9876543
 using namespace std;
 
 vector<pair<int, int>> edge[MAX];
 int Dist[MAX];
+int Parent[MAX];
+
+void Dijkstra(int Start){
+    priority_queue<pair<int, int>> PQ;
+    PQ.push({0, Start});
+    Dist[Start] = 0;
+    while(!PQ.empty()){
+        int Cost = -PQ.top().first;
+        int Cur = PQ.top().second;
+        PQ.pop();
+        if(Dist[Cur] < Cost) continue;
+        for(int i = 0; i < edge[Cur].size(); i++){
+            int Next = edge[Cur][i].first;
+            int nCost = edge[Cur][i].second;
+            if(Dist[Next] > Cost + nCost){
+                Dist[Next] = Cost + nCost;
+                Parent[Next] = Cur;
+                PQ.push({-Dist[Next], Next});
+            }
+        }
+    }
+}
 
 int main(){
     ios::sync_with_stdio(0);
@@ -17,5 +40,11 @@ int main(){
         cin >> src >> dst >> cost;
         edge[src].push_back({dst, cost});
         edge[dst].push_back({src, cost});
+    }
+    for(int i = 0; i < MAX; i++) Dist[i] = INF;
+    Dijkstra(1);
+    cout << N - 1 << '\n';
+    for(int i = 2; i <= N; i++){
+        cout << Parent[i] << " " << i << '\n';
     }
 }

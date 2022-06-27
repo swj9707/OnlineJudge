da626f5a05e4365e257b6c593637ff55bce00e9f
SierraSon97 <swj1265@naver.com>
2022-06-27 22:31:14 +0900

    20220627

---
+++ BOJ/Algorithms/DataStructure/boj12789.java
@@ -0,0 +1,18 @@
+package BOJ.Algorithms.DataStructure;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj12789 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        
+    }   
+}

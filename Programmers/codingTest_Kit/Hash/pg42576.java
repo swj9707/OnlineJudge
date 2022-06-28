5a2620e805ce66e00a26e61d38f1d87d4b806898
SierraSon97 <swj1265@naver.com>
2022-06-28 22:26:00 +0900

    20220628

---
+++ Programmers/codingTest_Kit/Hash/pg42576.java
@@ -0,0 +1,27 @@
+package Programmers.codingTest_Kit.Hash;
+
+import java.util.*;
+
+public class pg42576 {
+    public String solution(String[] participant, String[] completion){
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for(String player : completion){
+            map.put(player, map.get(player) - 1);
+        }
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+}

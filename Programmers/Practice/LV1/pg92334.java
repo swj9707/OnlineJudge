08354e4568acb3bfb50859a77e7b5763c808a35f
SierraSon97 <swj1265@naver.com>
2022-06-29 19:37:50 +0900

    20220629

---
+++ Programmers/Practice/LV1/pg92334.java
@@ -0,0 +1,38 @@
+package Programmers.Practice.LV1;
+
+import java.util.*;
+
+public class pg92334 {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> reportedMap = new HashMap<>();
+        Map<String, Integer> answerMap = new HashMap<>();
+
+        for(int i = 0; i < id_list.length; i++){
+            reportedMap.put(id_list[i], new HashSet<String>());
+            answerMap.put(id_list[i], 0);
+        }
+
+        for(String data : report){
+            String[] users = data.split(" ");
+            String reportingUser = users[0];
+            String reportedUser = users[1];
+            reportedMap.get(reportedUser).add(reportingUser);
+        }
+
+        for(String reportedUser : reportedMap.keySet()){
+            HashSet<String> userForSend = reportedMap.get(reportedUser);
+            if(userForSend.size() >= k){
+                for(String userId : userForSend){
+                    answerMap.put(userId, answerMap.get(userId) + 1);
+                }
+            }
+        }
+
+        for(int i = 0; i <id_list.length; i++){
+            answer[i] = answerMap.get(id_list[i]);
+        }
+        
+        return answer;
+    }
+}

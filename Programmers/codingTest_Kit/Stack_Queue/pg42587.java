c1f3c369f434a3b3b3ee5dc31414737db9b87303
SierraSon97 <swj1265@naver.com>
2022-06-29 17:38:45 +0900

    20220629

---
+++ Programmers/codingTest_Kit/Stack_Queue/pg42587.java
@@ -0,0 +1,31 @@
+package Programmers.codingTest_Kit.Stack_Queue;
+
+import java.util.*;
+
+public class pg42587 {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        PriorityQueue<Integer> PQ = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < priorities.length; i++){
+            PQ.add(priorities[i]);
+        }
+
+        while(!PQ.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(priorities[i] == PQ.peek()){
+                    if(i == location){
+                        answer++;
+                        return answer;
+                    }
+                }
+                PQ.poll();
+                answer++;
+            }
+        }
+    
+        return -1;
+    }
+    
+}

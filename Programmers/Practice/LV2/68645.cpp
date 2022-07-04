e79c62fcc906f0d401292ae85cc12d83133f516d
SierraSon97 <swj1265@naver.com>
2022-07-04 16:49:22 +0900

    20220704

---
+++ Programmers/Practice/LV2/68645.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#define MAX 1001
+using namespace std;
+
+int MATRIX[MAX][MAX];
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    return answer;
+}

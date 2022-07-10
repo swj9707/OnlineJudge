eac097a2a265950d9c8ffe6ed13dbe708839737d
SierraSon97 <swj1265@naver.com>
2022-07-10 19:53:25 +0900

    20220710

---
+++ BOJ/LearnJava/implements/back2557.java
@@ -0,0 +1,10 @@
+package BOJ.LearnJava.implements;
+
+import java.io.*;
+
+public class back2557 {
+    public static void main(String[] args){
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+    }   
+}

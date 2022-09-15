d8e5e1aae28c6472553f85a600f84239cbbddb33
swj9707 <swj1265@naver.com>
2022-09-15 22:57:07 +0900

    20220915

--- BOJ/Algorithms/Greedy/4889.cpp
+++ BOJ/Algorithms/Greedy/4889.cpp
@@ -5,6 +5,33 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+    string Str;
+    int caseNum = 1;
+    while(1){
+        cin >> Str;
+        if(Str[0] == '-'){
+            break;
+        }
+        int result = 0;
+        stack<char> stk;
+        for(auto i : Str){
+            if(i == '}' && !stk.empty() && stk.top() == '{'){
+                stk.pop();
+            } else {
+                stk.push(i);
+            }
+        }
 
+        while(!stk.empty()){
+            char first = stk.top();
+            stk.pop();
+            char second = stk.top();
+            stk.pop();
+            if(first == second) result++;
+            else result += 2;
+        }
+        cout << caseNum << ". " << result << '\n';
+        caseNum++;
+    }
+    return 0;
 }

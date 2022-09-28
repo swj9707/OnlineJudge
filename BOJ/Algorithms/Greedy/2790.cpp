36403a4e2bf404bd7c84905641827e0835c49005
swj9707 <swj1265@naver.com>
2022-09-28 23:28:24 +0900

    20220928

--- BOJ/Algorithms/Greedy/2790.cpp
+++ BOJ/Algorithms/Greedy/2790.cpp
@@ -6,5 +6,19 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int N; cin >> N;
+    int answer = 1;
+    vector<int> racer(N);
+    for(int i = 0; i < N; i++){
+        cin >> racer[i];
+    }
+    sort(racer.begin(), racer.end(), greater<int>());
+    int score = racer[0] + 1;
+    for(int i = 1; i < N; i++){
+        if(racer[i] + N >= score){
+            answer++;
+        }
+        score = max(score. racer[i] + i  + 1);
+    }
+    cout << answer << '\n';
     
 }

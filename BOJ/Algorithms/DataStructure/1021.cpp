66c3ec5080aa1c848a7e635a1cc16efd12273538
swj9707 <swj1265@naver.com>
2022-10-05 00:24:01 +0900

    20221005

--- BOJ/Algorithms/DataStructure/1021.cpp
+++ BOJ/Algorithms/DataStructure/1021.cpp
@@ -1,9 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+    int N, M;
+    int answer = 0;
+    cin >> N >> M;
+    deque<int> dq;
+    for (int i = 0; i < N; i++)
+    {
+        dq.push_back(i + 1);
+    }
+    queue<int> orderData;
+    for (int i = 0; i < M; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        orderData.push(tmp);
+    }
+    while (!orderData.empty())
+    {
+        if (dq.front() == orderData.front())
+        {
+            dq.pop_front();
+            orderData.pop();
+        }
+        else
+        {
+            int tmp = dq.front();
+            if (dq.front() > orderData.front())
+            {
+                dq.pop_back();
+                dq.push_front(tmp);
+            }
+            else
+            {
+                dq.pop_front();
+                dq.push_back(tmp);
+            }
+            answer++;
+        }
+    }
+    cout << answer << '\n';
 }

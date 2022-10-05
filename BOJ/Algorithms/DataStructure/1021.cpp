70a2dd340e2ed872cebb854ef1668d7ea6fcc528
swj9707 <swj1265@naver.com>
2022-10-05 23:21:07 +0900

    20221005

--- BOJ/Algorithms/DataStructure/1021.cpp
+++ BOJ/Algorithms/DataStructure/1021.cpp
@@ -10,38 +10,52 @@ int main()
     int answer = 0;
     cin >> N >> M;
     deque<int> dq;
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i <= N; i++)
     {
-        dq.push_back(i + 1);
+        dq.push_back(i);
     }
-    queue<int> orderData;
     for (int i = 0; i < M; i++)
     {
-        int tmp;
-        cin >> tmp;
-        orderData.push(tmp);
-    }
-    while (!orderData.empty())
-    {
-        if (dq.front() == orderData.front())
+        int num;
+        cin >> num;
+        int left = 0, right = 0;
+        for (int idx = 0; idx < dq.size(); idx++)
         {
+            if (dq[idx] == num)
+            {
+                left = idx;
+                right = dq.size() - idx;
+                break;
+            }
+        }
+        if (left <= right)
+        {
+            while (1)
+            {
+                if (dq.front() == num)
+                {
+                    break;
+                }
+                dq.push_back(dq.front());
+                dq.pop_front();
+                answer++;
+            }
             dq.pop_front();
-            orderData.pop();
         }
         else
         {
-            int tmp = dq.front();
-            if (dq.front() > orderData.front())
-            {
-                dq.pop_back();
-                dq.push_front(tmp);
-            }
-            else
-            {
-                dq.pop_front();
-                dq.push_back(tmp);
-            }
             answer++;
+            while (1)
+            {
+                if (dq.back() == num)
+                {
+                    break;
+                }
+                dq.push_front(dq.back());
+                dq.pop_back();
+                answer++;
+            }
+            dq.pop_back();
         }
     }
     cout << answer << '\n';

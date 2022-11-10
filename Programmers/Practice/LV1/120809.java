e494f5fb331a3942a411bc7b7f83c226f44125a9
swj9707 <swj1265@naver.com>
2022-11-10 20:35:56 +0900

    20221110

---
+++ Programmers/Practice/LV1/120809.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] * 2;
+        }
+        answer = numbers;
+
+        return answer;
+    }
+}

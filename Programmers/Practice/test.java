fbe4cf6b2818f1766b79bbc04f2937bafa837de1
swj9707 <swj1265@naver.com>
2022-11-11 21:47:43 +0900

    20221111

--- Programmers/Practice/test.java
+++ Programmers/Practice/test.java
@@ -1,11 +1,21 @@
 class Solution {
-    public int solution(int[] numbers) {
+    public int solution(int n) {
         int answer = 0;
+        int[] data = new int[101];
 
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < i; j++) {
-                answer = Math.max(answer, numbers[i] * numbers[j]);
+        data[0] = data[1] = 1;
+
+        for (int i = 2; i * i <= 100; i++) {
+            if (data[i] == 0) {
+                for (int j = i * i; j <= 100; j += i) {
+                    data[j] = 1;
+                }
             }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (data[i] == 1 && i != 1)
+                answer++;
         }
 
         return answer;

e4a9ab7e2d982035679cb349546f10d4a341a79f
swj9707 <swj1265@naver.com>
2022-11-11 22:03:16 +0900

    20221111

--- Programmers/Practice/test.java
+++ Programmers/Practice/test.java
@@ -1,21 +1,10 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        int[] data = new int[101];
+    public String solution(String my_string) {
+        String answer = "";
 
-        data[0] = data[1] = 1;
-
-        for (int i = 2; i * i <= 100; i++) {
-            if (data[i] == 0) {
-                for (int j = i * i; j <= 100; j += i) {
-                    data[j] = 1;
-                }
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            if (data[i] == 1 && i != 1)
-                answer++;
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.indexOf(my_string.charAt(i)) == i)
+                answer += my_string.charAt(i);
         }
 
         return answer;

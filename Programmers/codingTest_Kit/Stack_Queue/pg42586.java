0165d7ced144b65f016c0eee0b789b869bd37639
swj9707 <swj1265@naver.com>
2022-11-15 19:45:19 +0900

    20221115

--- Programmers/codingTest_Kit/Stack_Queue/pg42586.java
+++ Programmers/codingTest_Kit/Stack_Queue/pg42586.java
@@ -1,44 +1,39 @@
 package Programmers.codingTest_Kit.Stack_Queue;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-public class pg42586 {
-    class Solution {
-        public int[] solution(int[] progresses, int[] speeds) {
-           
-            Queue<Integer> queue = new LinkedList<>();
-            List<Integer> answerList = new ArrayList<>();
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
 
-            for(int i = 0; i < progresses.length; i++){
-                int leftRate = 100 - progresses[i];
-                int date = leftRate / speeds[i];
-                if(leftRate % speeds[i] != 0) date += 1;
-                queue.offer(date);
-            }
-            int start = queue.peek();
-            int count = 0;
+        for (int i = 0; i < progresses.length; i++) {
+            int left = 100 - progresses[i];
+            int date = left / speeds[i];
+            if (left % speeds[i] != 0)
+                date += 1;
+            q.offer(date);
+        }
 
-            while(!queue.isEmpty()){
-                if(queue.peek() <= start){
-                    queue.remove();
-                    count++;
-                } else {
-                    if(count > 0){
-                        start = queue.peek();
-                        answerList.add(count);
-                        count = 0;
-                    }
+        int startDate = q.peek();
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            if (q.peek() <= startDate) {
+                q.remove();
+                count++;
+            } else {
+                if (count > 0) {
+                    startDate = q.peek();
+                    result.add(count);
+                    count = 0;
                 }
             }
-            answerList.add(count);
-
-            int[] answer = new int[answerList.size()];
-
-            for(int i = 0; i < answerList.size(); i++){
-                answer[i] = answerList.get(i);
-            }
-
-            return answer;
         }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }

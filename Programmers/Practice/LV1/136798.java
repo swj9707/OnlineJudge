f021442ce5c01fce753ad61d2a54e2edc3b67031
swj9707 <swj1265@naver.com>
2022-11-18 16:41:32 +0900

    20221118

---
+++ Programmers/Practice/LV1/136798.java
@@ -0,0 +1,8 @@
+package Programmers.Practice.LV1;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+}
--- Programmers/Practice/LV2/92342.java
+++ Programmers/Practice/LV2/92342.java
@@ -1,9 +1,46 @@
-package LV2;
+package Programmers.Practice.LV2;
 
 class Solution {
-    public int[] solution(int n, int[] info) {
-        int[] answer = {};
+    static int max;
+    static int[] answer;
 
-        return answer;
+    public int[] solution(int n, int[] info) {
+        answer = new int[11];
+        DFS(0, n, 0, new int[n], info);
+
+        return max == 0 ? new int[] { -1 } : answer;
+    }
+
+    public static void DFS(int start, int r, int idx, int[] result, int[] info) {
+        if (idx == r) {
+            int[] info2 = new int[11];
+            for (int score : result) {
+                info2[10 - score]++;
+            }
+            int ryan = 0;
+            int apeach = 0;
+
+            for (int i = 0; i < 11; i++) {
+                int score = 10 - i;
+                if (info2[i] > info[i]) {
+                    ryan += score;
+                } else if (info2[i] < info[i]) {
+                    apeach += score;
+                } else if (info[i] != 0 && info2[i] == info[i]) {
+                    apeach += score;
+                }
+            }
+
+            if (ryan - apeach > max) {
+                max = ryan - apeach;
+                answer = info2;
+            }
+            return;
+        }
+
+        for (int i = start; i < 11; i++) {
+            result[idx] = i;
+            DFS(i, r, idx + 1, result, info);
+        }
     }
 }

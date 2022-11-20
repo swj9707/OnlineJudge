78362d8a287e0cc7cc1f35e9d56824869e99ad4e
swj9707 <swj1265@naver.com>
2022-11-20 17:58:41 +0900

    20221120

--- Programmers/Practice/LV1/136798.java
+++ Programmers/Practice/LV1/136798.java
@@ -2,7 +2,20 @@ package Programmers.Practice.LV1;
 
 class Solution {
     public int solution(int number, int limit, int power) {
+        int[] data = new int[number + 1];
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                data[i * j]++;
+            }
+        }
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (data[i] > limit) {
+                answer += power;
+            } else {
+                answer += data[i];
+            }
+        }
         return answer;
     }
 }

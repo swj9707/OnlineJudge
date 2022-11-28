5fe722008ab4da32c9bba8982ef871568a481752
swj9707 <swj1265@naver.com>
2022-11-28 20:22:58 +0900

    20221128

---
+++ Programmers/Practice/LV2/138476.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for( int i : tangerine){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
+
+        for(Integer x : list){
+            k -= map.get(x);
+            answer++;
+            if(k <= 0){
+                break;
+            }
+        }
+
+
+        return answer;
+    }
+}

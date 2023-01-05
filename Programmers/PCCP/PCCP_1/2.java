2729f34a408a3d9f8241a23421df7e5849eab32a
swj9707 <swj1265@naver.com>
2023-01-05 19:51:23 +0900

    20230105

--- Programmers/PCCP/PCCP_1/2.java
+++ Programmers/PCCP/PCCP_1/2.java
@@ -1,8 +1,33 @@
 package Programmers.PCCP.PCCP_1;
 
 class Solution {
+
+    boolean[] VISIT = new boolean[10];
+    int[] MATRIX = new int[10];
+    int maxValue = -1;
+
     public int solution(int[][] ability) {
-        int answer = 0;
-        return answer;
+        DFS(0, ability[0].length, ability.length, ability);
+        return maxValue;
+    }
+
+    public void DFS(int count, int n, int max, int[][] ability) {
+        if(count == n){
+            int result = 0;
+            for(int i =0; i < n; i++){
+                result += ability[MATRIX[i]][i];
+            }
+            maxValue = Math.max(maxValue, result);
+            return;
+        }
+
+        for(int i = 0; i < max; i++){
+            if(!VISIT[i]){
+                VISIT[i] = true;
+                MATRIX[count] = i;
+                DFS(count+1, n, max, ability);
+                VISIT[i] = false;
+            }
+        }
     }
 }

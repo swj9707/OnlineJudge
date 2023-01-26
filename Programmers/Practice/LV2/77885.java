fe06a9c6c1af8b7f270320d0ac8ee23f3b615de2
swj9707 <swj1265@naver.com>
2023-01-26 20:23:31 +0900

    20230126

---
+++ Programmers/Practice/LV2/77885.java
@@ -0,0 +1,12 @@
+package Programmers.Practice.LV2;
+
+class Solution {
+    public long[] solution(long[] numbers) {
+        long[] answer = numbers.clone();
+        for(int i = 0; i< answer.length; i++){
+            answer[i]++;
+            answer[i] += (answer[i]^numbers[i])>>2;
+        }
+        return answer;
+    }
+}

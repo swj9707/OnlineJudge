38da9f6c26d47578bd7ff166a8d8eb7ec7f75ba3
swj9707 <swj1265@naver.com>
2023-02-14 23:19:23 +0900

    20230214

---
+++ BOJ/Algorithms/Greedy/16120.java
@@ -0,0 +1,20 @@
+package BOJ.Algorithms.Greedy;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String tmp = br.readLine();
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < tmp.length(); i++) {
+            stk.push(tmp.charAt(i));
+            if (stk.size() >= 4) {
+                for()
+            }
+        }
+    }
+}

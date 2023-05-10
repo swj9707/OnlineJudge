0dd83fa9a59f6870dd8f45777b013bc25976da9b
WOOJIN SON <swj1265@naver.com>
2023-05-11 00:28:53 +0900

    20230511

---
+++ leetcode/TopInterview/Array_String/88.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (m + n > m) {
+            for (int i = m; i < m + n; i++) {
+                nums1[i] = nums2[i - m];
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
